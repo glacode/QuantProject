8c4d25be3e59e2fbfeeea88ba430f715c50b1492
glauco_1 <>
2003-11-09 19:44:40 +0000

    First version for the max drawdown calculation

---
+++ QuantProject/b4_Business/a1_Financial/a2_Accounting/h5_Reporting/SummaryRows/MaxEquityDrawDown.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Data;
+using QuantProject.Business.Financial.Accounting.Reporting.Tables;
+
+namespace QuantProject.Business.Financial.Accounting.Reporting.SummaryRows
+{
+	/// <summary>
+	/// Summary description for TotalNumberOfTrades.
+	/// </summary>
+	public class MaxEquityDrawDown : SummaryRow
+	{
+    private Summary summary;
+    private double drawDown;
+    private double maxEquityValue;
+    private void updateDrawDownForCurrentRow( DataRow dataRow )
+    {
+      this.maxEquityValue = Math.Max( this.maxEquityValue , (double)dataRow[ "AccountValue" ] );
+      this.drawDown =
+        Math.Max( this.drawDown , this.maxEquityValue - (double)dataRow[ "AccountValue" ] );
+    }
+    private void setDrawDown()
+    {
+      drawDown = 0.0;
+      maxEquityValue = double.MinValue;
+      foreach ( DataRow row in summary.AccountReport.Equity.DataTable.Rows )
+        updateDrawDownForCurrentRow( row );
+    }
+		public MaxEquityDrawDown( Summary summary )
+		{
+      this.summary = summary;
+      this.rowDescription = "Max equity drawdown (%)";
+      setDrawDown();
+      this.rowValue = this.drawDown/this.maxEquityValue*100;
+
+    }
+	}
+}
