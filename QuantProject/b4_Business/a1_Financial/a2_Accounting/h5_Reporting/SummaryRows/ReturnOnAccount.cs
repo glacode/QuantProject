b5a2a969024489cd27a9bbc32278fff1e9332b4d
glauco_1 <>
2003-11-10 21:35:52 +0000

    Computes the return on account for the account report summary object

---
+++ QuantProject/b4_Business/a1_Financial/a2_Accounting/h5_Reporting/SummaryRows/ReturnOnAccount.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Data;
+using QuantProject.Business.Financial.Accounting.Reporting.Tables;
+
+namespace QuantProject.Business.Financial.Accounting.Reporting.SummaryRows
+{
+	/// <summary>
+	/// Summary description for TotalNumberOfTrades.
+	/// </summary>
+	public class ReturnOnAccount : SummaryRow
+	{
+		public ReturnOnAccount( Summary summary )
+		{
+      this.rowDescription = "Return on account";
+      this.rowValue = summary.TotalPnl / ( summary.FinalAccountValue - summary.TotalPnl ) * 100;
+    }
+	}
+}
