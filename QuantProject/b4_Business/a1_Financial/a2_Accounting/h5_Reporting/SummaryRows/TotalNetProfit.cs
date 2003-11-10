4aa9bb190344ddc9df3f5aba0f8491ff8ce8229d
glauco_1 <>
2003-11-10 21:36:36 +0000

    Computes the total net profit for the account report summary object

---
+++ QuantProject/b4_Business/a1_Financial/a2_Accounting/h5_Reporting/SummaryRows/TotalNetProfit.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Data;
+using QuantProject.Business.Financial.Accounting.Reporting.Tables;
+
+namespace QuantProject.Business.Financial.Accounting.Reporting.SummaryRows
+{
+	/// <summary>
+	/// Summary description for TotalNumberOfTrades.
+	/// </summary>
+	public class TotalNetProfit : SummaryRow
+	{
+		public TotalNetProfit( Summary summary )
+		{
+      this.rowDescription = "Total net profit";
+      this.rowValue = summary.TotalPnl;
+    }
+	}
+}
