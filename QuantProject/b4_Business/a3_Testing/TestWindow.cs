603229407952d8d05a27a30a77cdb2c611cbfb41
glauco_1 <>
2003-11-24 20:11:58 +0000

    - Fixed the class namespace

--- QuantProject/b4_Business/a3_Testing/TestWindow.cs
+++ QuantProject/b4_Business/a3_Testing/TestWindow.cs
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 using System;
 
-namespace QuantProject.Business.Financial.Testing
+namespace QuantProject.Business.Testing
 {
   /// <summary>
   /// Summary description for TestWindow.
