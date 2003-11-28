0ceba67d15a579844d6176e4fd024bae7588b876
glauco_1 <>
2003-11-28 15:52:31 +0000

    -The BackTester class now is an abstract class - A public Name property has been added to the BackTester class

--- QuantProject/b4_Business/a3_Testing/BackTester.cs
+++ QuantProject/b4_Business/a3_Testing/BackTester.cs
@@ -2,22 +2,26 @@ using System;
 using QuantProject.ADT.Optimizing;
 using QuantProject.ADT;
 using QuantProject.Business.Financial.Accounting;
+using QuantProject.Business.Strategies;
 
 namespace QuantProject.Business.Testing
 {
 	/// <summary>
 	/// Summary description for BackTester.
 	/// </summary>
-	public class BackTester : Optimizable
+	public abstract class BackTester : Optimizable
 	{
     private Account account = new Account();
+    public string Name;
 
     public Account Account
     {
       get { return account; }
       set { account = value; }
     }
-		public BackTester()
+    public TradingSystems TradingSystems = new TradingSystems();
+
+    public BackTester()
 		{
 			//
 			// TODO: Add constructor logic here
@@ -27,5 +31,6 @@ namespace QuantProject.Business.Testing
     {
       return 0;
     }
+    public abstract void Test();
 	}
 }
