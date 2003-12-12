76f234e37ec25875c77f157a735973d22e4b378e
glauco_1 <>
2003-12-12 00:14:39 +0000

    Data validation has been moved to the QuotesToBeValidated object. Suspicious data rows now fire an event in the QuotesToBeValidated object: this event is handled by ValidateDataTable.

--- QuantDownloader/Downloader/Validate/ValidateDataTable.cs
+++ QuantDownloader/Downloader/Validate/ValidateDataTable.cs
@@ -25,68 +25,26 @@ namespace QuantProject.Applications.Downloader.Validate
       this.oleDbDataAdapter.Fill( this );
     }
 
-    #region "addRows_forCurrentQuotesRow"
     /// <summary>
     /// Adds quotesRow to the ValidateDataTable
     /// </summary>
     /// <param name="quotesRow">Row of quotes to added</param>
-    private void addRow( DataRow quotesRow )
+    private void suspiciousDataRowEventHandler( Object sender ,
+      SuspiciousDataRowEventArgs eventArgs )
     {
+      DataRow quotesRow = eventArgs.DataRow;
       DataRow dataRow = this.NewRow();
       foreach (DataColumn dataColumn in quotesRow.Table.Columns )
         dataRow[ dataColumn.ColumnName ] = quotesRow[ dataColumn ];
       this.Rows.Add( dataRow );
       //this.Rows.Add( quotesRow );
     }
-    #region "addRows_forCurrentQuotesRow_checkLogicalErrors"
-    /// <summary>
-    /// Adds a row if not ((Low <= Open) and (Open <= High) and (Low <= Close) and (Close <= High))
-    /// </summary>
-    /// <param name="quotesRow">Row of quotes to be checked</param>
-    private void checkOHLC( DataRow quotesRow )
-    {
-      if (!
-          ( ( Convert.ToDouble( quotesRow[ "quLow" ] ) <=
-              ( Convert.ToDouble( quotesRow[ "quOpen" ] ) ) ) &&
-            ( Convert.ToDouble( quotesRow[ "quOpen" ] ) <=
-              ( Convert.ToDouble( quotesRow[ "quHigh" ] ) ) ) &&
-            ( Convert.ToDouble( quotesRow[ "quLow" ] ) <=
-              ( Convert.ToDouble( quotesRow[ "quClose" ] ) ) ) &&
-            ( Convert.ToDouble( quotesRow[ "quClose" ] ) <=
-              ( Convert.ToDouble( quotesRow[ "quHigh" ] ) ) )
-          )
-         ) 
-        addRow( quotesRow );
-    }
-    /// <summary>
-    /// Adds an error row if quotesRow doesn't respect logical constraints
-    /// </summary>
-    /// <param name="quotesRow">Row of quotes to be checked</param>
-    private void addRows_forCurrentQuotesRow_checkLogicalErrors( DataRow quotesRow )
-    {
-      checkOHLC( quotesRow );
-    }
-    #endregion
-    /// <summary>
-    /// Adds errors for the current quotesRow (if any)
-    /// </summary>
-    /// <param name="quotesRow">Row of quotes to be checked</param>
-    private void addRows_forCurrentQuotesRow( DataRow quotesRow )
-    {
-      addRows_forCurrentQuotesRow_checkLogicalErrors( quotesRow );
-    }
-    #endregion
-
-    /// <summary>
-    /// Adds all the probably wrong data quotes rows
-    /// </summary>
-    /// <param name="tickerIsLike">Contains the is like clause to fetch the tickers
-    /// whose quotes are to be checked</param>
     public void AddRows( string tickerIsLike )
     {
       QuotesToBeValidated quotesToBeValidated = new QuotesToBeValidated( tickerIsLike );
-      foreach ( DataRow quotesRow in quotesToBeValidated.Rows )
-        this.addRows_forCurrentQuotesRow( quotesRow );
+      quotesToBeValidated.SuspiciousDataRow +=
+        new QuotesToBeValidated.SuspiciousDataRowEventHandler( suspiciousDataRowEventHandler );
+      quotesToBeValidated.Validate();
       this.AcceptChanges();
     }
 
