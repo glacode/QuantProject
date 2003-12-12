1b8078e102205f51caa5baeaa240a3044313cfa6
glauco_1 <>
2003-12-12 00:21:05 +0000

    It will contain the interface for quotes validators. Not defined yet.

---
+++ QuantDownloader/Downloader/Validate/IValidator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace QuantProject.Applications.Downloader.Validate
+{
+	/// <summary>
+	/// Interface to be implemented by quotes validators
+	/// </summary>
+	public interface IValidator
+	{
+		//event SuspiciousDataRow
+	}
+}
