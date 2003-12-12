0aeba784ad97dd3bb28f59050760c1b24c7b7ebe
glauco_1 <>
2003-12-12 00:16:14 +0000

    Data validation has been moved to this object. Every suspicious data rows fires the event SuspiciousDataRow.

--- QuantDownloader/Downloader/Validate/QuotesToBeValidated.cs
+++ QuantDownloader/Downloader/Validate/QuotesToBeValidated.cs
@@ -28,5 +28,55 @@ namespace QuantProject.Applications.Downloader.Validate
         Console.WriteLine( exception.ToString() );
       }
     }
+
+    public delegate void SuspiciousDataRowEventHandler(
+      Object sender , SuspiciousDataRowEventArgs eventArgs );
+
+    public event SuspiciousDataRowEventHandler SuspiciousDataRow;
+
+    #region "Validate"
+    #region "validate_currentQuotesRow_checkLogicalErrors"
+    /// <summary>
+    /// Adds a row if not ((Low <= Open) and (Open <= High) and (Low <= Close) and (Close <= High))
+    /// </summary>
+    /// <param name="quotesRow">Row of quotes to be checked</param>
+    private void validate_currentQuotesRow_checkLogicalErrors_checkOHLC( DataRow quotesRow )
+    {
+      if (!
+        ( ( Convert.ToDouble( quotesRow[ "quLow" ] ) <=
+        ( Convert.ToDouble( quotesRow[ "quOpen" ] ) ) ) &&
+        ( Convert.ToDouble( quotesRow[ "quOpen" ] ) <=
+        ( Convert.ToDouble( quotesRow[ "quHigh" ] ) ) ) &&
+        ( Convert.ToDouble( quotesRow[ "quLow" ] ) <=
+        ( Convert.ToDouble( quotesRow[ "quClose" ] ) ) ) &&
+        ( Convert.ToDouble( quotesRow[ "quClose" ] ) <=
+        ( Convert.ToDouble( quotesRow[ "quHigh" ] ) ) )
+        )
+        ) 
+        SuspiciousDataRow( this , new SuspiciousDataRowEventArgs( quotesRow ) );
+    }
+    /// <summary>
+    /// Adds an error row if quotesRow doesn't respect logical constraints
+    /// </summary>
+    /// <param name="quotesRow">Row of quotes to be checked</param>
+    private void validate_currentQuotesRow_checkLogicalErrors( DataRow quotesRow )
+    {
+      validate_currentQuotesRow_checkLogicalErrors_checkOHLC( quotesRow );
+    }
+    #endregion
+    /// <summary>
+    /// Adds errors for the current quotesRow (if any)
+    /// </summary>
+    /// <param name="quotesRow">Row of quotes to be checked</param>
+    private void validate_currentQuotesRow( DataRow quotesRow )
+    {
+      validate_currentQuotesRow_checkLogicalErrors( quotesRow );
+    }
+    public void Validate()
+    {
+      foreach ( DataRow quotesRow in this.Rows )
+        this.validate_currentQuotesRow( quotesRow );
+    }
 	}
+  #endregion
 }
