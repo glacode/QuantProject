159ae3cfe3a5aee076903a16743a0bad572e8674
glauco_1 <>
2003-12-16 15:50:08 +0000

    A Validator class must implement this iterface

--- QuantDownloader/Downloader/Validate/IValidator.cs
+++ QuantDownloader/Downloader/Validate/IValidator.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Data;
 
 namespace QuantProject.Applications.Downloader.Validate
 {
+  public delegate void SuspiciousDataRowEventHandler(
+  Object sender , SuspiciousDataRowEventArgs eventArgs );
+
 	/// <summary>
 	/// Interface to be implemented by quotes validators
 	/// </summary>
 	public interface IValidator
 	{
-		//event SuspiciousDataRow
-	}
+    event SuspiciousDataRowEventHandler SuspiciousDataRow;
+    void Validate( DataTable dataTable );
+  }
 }
