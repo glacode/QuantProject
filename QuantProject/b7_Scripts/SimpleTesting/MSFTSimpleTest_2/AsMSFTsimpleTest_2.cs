23e4640d69e19d9f71c50f70eb9e3a5874061dca
millo_1 <>
2003-12-20 18:31:36 +0000

    Added new account strategy for MSFTsimpleTest_2 (not implemented yet)

---
+++ QuantProject/b7_Scripts/SimpleTesting/MSFTSimpleTest_2/AsMSFTsimpleTest_2.cs
@@ -0,0 +1,45 @@
+/*
+QuantProject - Quantitative Finance Library
+
+AsMSFTsimpleTest_2.cs
+Copyright (C) 2003 
+Marco Milletti
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+using System;
+using System.Collections;
+using QuantProject.ADT;
+using QuantProject.Business.Financial.Ordering;
+using QuantProject.Business.Financial.Accounting;
+using QuantProject.Business.Strategies;
+
+namespace QuantProject.Scripts
+{
+	/// <summary>
+	/// Summary description for AsMSFTsimpleTest_2.
+	/// </summary>
+	public class AsMSFTsimpleTest_2 : AccountStrategy
+	{
+		public AsMSFTsimpleTest_2( Account account ) : base( account )
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+    }
+	//TODO: override of the base methods
+}
