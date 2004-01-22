b80c04c18808faf198ffdcdc18d5a69873465c06
millo_1 <>
2004-01-22 23:51:49 +0000

    Added new menu item to call TickerViewer object

--- QuantDownloader/Downloader/Main.cs
+++ QuantDownloader/Downloader/Main.cs
@@ -23,6 +23,9 @@ namespace QuantProject.Applications.Downloader
     private System.Windows.Forms.MenuItem subMenuFromTheWeb;
     private System.Windows.Forms.MenuItem menuValidate;
     private System.Windows.Forms.MenuItem subMenuValidateGo;
+		private System.Windows.Forms.MenuItem menuItemOpen;
+		private System.Windows.Forms.MenuItem menuItemTickerViewer;
+		private System.Windows.Forms.MenuItem menuItemTickerGroupsViewer;
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
@@ -62,95 +65,118 @@ namespace QuantProject.Applications.Downloader
 		/// </summary>
 		private void InitializeComponent()
 		{
-      this.mainMenu1 = new System.Windows.Forms.MainMenu();
-      this.menuImport = new System.Windows.Forms.MenuItem();
-      this.subMenuFromTheWeb = new System.Windows.Forms.MenuItem();
-      this.menuValidate = new System.Windows.Forms.MenuItem();
-      this.subMenuValidateGo = new System.Windows.Forms.MenuItem();
-      this.menuItem1 = new System.Windows.Forms.MenuItem();
-      this.menuItem2 = new System.Windows.Forms.MenuItem();
-      this.menuItem5 = new System.Windows.Forms.MenuItem();
-      this.menuItem3 = new System.Windows.Forms.MenuItem();
-      this.menuItem4 = new System.Windows.Forms.MenuItem();
-      this.menuItem6 = new System.Windows.Forms.MenuItem();
-      this.menuItem7 = new System.Windows.Forms.MenuItem();
-      // 
-      // mainMenu1
-      // 
-      this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
-                                                                              this.menuImport,
-                                                                              this.menuValidate});
-      // 
-      // menuImport
-      // 
-      this.menuImport.Index = 0;
-      this.menuImport.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
-                                                                               this.subMenuFromTheWeb});
-      this.menuImport.Text = "Import";
-      // 
-      // subMenuFromTheWeb
-      // 
-      this.subMenuFromTheWeb.Index = 0;
-      this.subMenuFromTheWeb.Text = "From the Web";
-      this.subMenuFromTheWeb.Click += new System.EventHandler(this.menuItem10_Click);
-      // 
-      // menuValidate
-      // 
-      this.menuValidate.Index = 1;
-      this.menuValidate.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
-                                                                                 this.subMenuValidateGo});
-      this.menuValidate.Text = "Validate";
-      // 
-      // subMenuValidateGo
-      // 
-      this.subMenuValidateGo.Index = 0;
-      this.subMenuValidateGo.Text = "Go";
-      this.subMenuValidateGo.Click += new System.EventHandler(this.subMenuValidateGo_Click);
-      // 
-      // menuItem1
-      // 
-      this.menuItem1.Index = -1;
-      this.menuItem1.Text = "";
-      // 
-      // menuItem2
-      // 
-      this.menuItem2.Index = -1;
-      this.menuItem2.Text = "";
-      // 
-      // menuItem5
-      // 
-      this.menuItem5.Index = -1;
-      this.menuItem5.Text = "";
-      // 
-      // menuItem3
-      // 
-      this.menuItem3.Index = -1;
-      this.menuItem3.Text = "";
-      // 
-      // menuItem4
-      // 
-      this.menuItem4.Index = -1;
-      this.menuItem4.Text = "";
-      // 
-      // menuItem6
-      // 
-      this.menuItem6.Index = -1;
-      this.menuItem6.Text = "";
-      // 
-      // menuItem7
-      // 
-      this.menuItem7.Index = -1;
-      this.menuItem7.Text = "";
-      // 
-      // Principale
-      // 
-      this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-      this.ClientSize = new System.Drawing.Size(292, 273);
-      this.Menu = this.mainMenu1;
-      this.Name = "Principale";
-      this.Text = "Principale";
+			this.mainMenu1 = new System.Windows.Forms.MainMenu();
+			this.menuItemOpen = new System.Windows.Forms.MenuItem();
+			this.menuItemTickerViewer = new System.Windows.Forms.MenuItem();
+			this.menuItemTickerGroupsViewer = new System.Windows.Forms.MenuItem();
+			this.menuImport = new System.Windows.Forms.MenuItem();
+			this.subMenuFromTheWeb = new System.Windows.Forms.MenuItem();
+			this.menuValidate = new System.Windows.Forms.MenuItem();
+			this.subMenuValidateGo = new System.Windows.Forms.MenuItem();
+			this.menuItem1 = new System.Windows.Forms.MenuItem();
+			this.menuItem2 = new System.Windows.Forms.MenuItem();
+			this.menuItem5 = new System.Windows.Forms.MenuItem();
+			this.menuItem3 = new System.Windows.Forms.MenuItem();
+			this.menuItem4 = new System.Windows.Forms.MenuItem();
+			this.menuItem6 = new System.Windows.Forms.MenuItem();
+			this.menuItem7 = new System.Windows.Forms.MenuItem();
+			// 
+			// mainMenu1
+			// 
+			this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+																					  this.menuItemOpen,
+																					  this.menuImport,
+																					  this.menuValidate});
+			// 
+			// menuItemOpen
+			// 
+			this.menuItemOpen.Index = 0;
+			this.menuItemOpen.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+																						 this.menuItemTickerViewer,
+																						 this.menuItemTickerGroupsViewer});
+			this.menuItemOpen.Text = "Open";
+			// 
+			// menuItemTickerViewer
+			// 
+			this.menuItemTickerViewer.Index = 0;
+			this.menuItemTickerViewer.Text = "Ticker Viewer";
+			this.menuItemTickerViewer.Click += new System.EventHandler(this.menuItemTickerViewer_Click);
+			// 
+			// menuItemTickerGroupsViewer
+			// 
+			this.menuItemTickerGroupsViewer.Index = 1;
+			this.menuItemTickerGroupsViewer.Text = "Ticker-Groups Viewer";
+			// 
+			// menuImport
+			// 
+			this.menuImport.Index = 1;
+			this.menuImport.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+																					   this.subMenuFromTheWeb});
+			this.menuImport.Text = "Import";
+			// 
+			// subMenuFromTheWeb
+			// 
+			this.subMenuFromTheWeb.Index = 0;
+			this.subMenuFromTheWeb.Text = "From the Web";
+			this.subMenuFromTheWeb.Click += new System.EventHandler(this.menuItem10_Click);
+			// 
+			// menuValidate
+			// 
+			this.menuValidate.Index = 2;
+			this.menuValidate.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+																						 this.subMenuValidateGo});
+			this.menuValidate.Text = "Validate";
+			// 
+			// subMenuValidateGo
+			// 
+			this.subMenuValidateGo.Index = 0;
+			this.subMenuValidateGo.Text = "Go";
+			this.subMenuValidateGo.Click += new System.EventHandler(this.subMenuValidateGo_Click);
+			// 
+			// menuItem1
+			// 
+			this.menuItem1.Index = -1;
+			this.menuItem1.Text = "";
+			// 
+			// menuItem2
+			// 
+			this.menuItem2.Index = -1;
+			this.menuItem2.Text = "";
+			// 
+			// menuItem5
+			// 
+			this.menuItem5.Index = -1;
+			this.menuItem5.Text = "";
+			// 
+			// menuItem3
+			// 
+			this.menuItem3.Index = -1;
+			this.menuItem3.Text = "";
+			// 
+			// menuItem4
+			// 
+			this.menuItem4.Index = -1;
+			this.menuItem4.Text = "";
+			// 
+			// menuItem6
+			// 
+			this.menuItem6.Index = -1;
+			this.menuItem6.Text = "";
+			// 
+			// menuItem7
+			// 
+			this.menuItem7.Index = -1;
+			this.menuItem7.Text = "";
+			// 
+			// Principale
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(232, 177);
+			this.Menu = this.mainMenu1;
+			this.Name = "Principale";
+			this.Text = "Principale";
 
-    }
+		}
 		#endregion
 
     private void menuItem9_Click(object sender, System.EventArgs e)
@@ -198,6 +224,12 @@ namespace QuantProject.Applications.Downloader
         new QuantProject.Applications.Downloader.Validate.ValidateForm();
       validateForm.ShowDialog();
     }
+
+		private void menuItemTickerViewer_Click(object sender, System.EventArgs e)
+		{
+			TickerViewer tickerViewer = new TickerViewer();
+			tickerViewer.Show();
+		}
 
 	}
 }
