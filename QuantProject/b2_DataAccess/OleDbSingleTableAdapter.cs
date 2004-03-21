433845e0dad00d1c89ef34f9ed2bbd09f9ac9e20
glauco_1 <>
2004-03-21 16:11:11 +0000

    Returns a complete OleDbDataAdapter (with proper edit commands already set) to work with the given table

---
+++ QuantProject/b2_DataAccess/OleDbSingleTableAdapter.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace QuantProject.DataAccess
+{
+	/// <summary>
+	/// Returns a complete OleDbDataAdapter (with proper edit commands already set) to work with
+	/// the given table
+	/// </summary>
+	public class OleDbSingleTableAdapter
+	{
+		private string tableName;
+		public string TableName
+		{
+			get { return this.tableName; }
+			set { this.tableName = value; }
+		}
+		private OleDbDataAdapter oleDbDataAdapter;
+		public OleDbDataAdapter OleDbDataAdapter
+		{
+			get { return this.oleDbDataAdapter; }
+		}
+		
+		private DataTable dataTable;
+		public DataTable DataTable
+		{
+			get { return this.dataTable; }
+		}
+		
+		public OleDbSingleTableAdapter( string selectStatement )
+		{
+			try
+			{
+//				string selectStatement =
+//					"select * from " + tableName + " where 1=2";
+				this.oleDbDataAdapter =
+					new OleDbDataAdapter( selectStatement , ConnectionProvider.OleDbConnection );
+				OleDbCommandBuilder oleDbCommandBuilder = new OleDbCommandBuilder( oleDbDataAdapter );
+				this.oleDbDataAdapter.InsertCommand = oleDbCommandBuilder.GetInsertCommand();
+				this.oleDbDataAdapter.UpdateCommand = oleDbCommandBuilder.GetUpdateCommand();
+				this.oleDbDataAdapter.DeleteCommand = oleDbCommandBuilder.GetDeleteCommand();
+				this.dataTable = new DataTable();
+				this.oleDbDataAdapter.Fill( this.dataTable );
+			}
+			catch ( Exception ex )
+			{
+				string exceptionMessage = ex.Message + "\n" + ex.StackTrace;
+				Console.WriteLine( exceptionMessage );
+			}
+		}
+
+//		public void SetAdapterAndDataTable( string tableName )
+//		{
+//		}
+	}
+}
