2d13230296a2899eb6fa20be44ff6df3ae0d3dec
glauco_1 <>
2004-03-28 19:45:23 +0000

    Updated, but it is going to be removed, because the visuallyValidatedTickers table has been removed from the database

--- QuantProject/b2_DataAccess/Tables/VisuallyValidatedTickers.cs
+++ QuantProject/b2_DataAccess/Tables/VisuallyValidatedTickers.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Data;
+using QuantProject.ADT;
 using QuantProject.DataAccess;
 
 namespace QuantProject.DataAccess.Tables
@@ -30,9 +31,10 @@ namespace QuantProject.DataAccess.Tables
 		{
 			try
 			{
+				Quotes quotes = new Quotes( ticker );
 				OleDbSingleTableAdapter oleDbSingleTableAdapter =
 					new OleDbSingleTableAdapter( "select * from visuallyValidatedTickers where vvTicker='" + ticker + "'" );
-				string hashValue = Quotes.GetHashValue( ticker ); 
+				string hashValue = quotes.GetHashValue(); 
 				if ( oleDbSingleTableAdapter.DataTable.Rows.Count == 0 )
 					// this ticker was not visually validated yet
 					oleDbSingleTableAdapter.DataTable.Rows.Add( oleDbSingleTableAdapter.DataTable.NewRow() );
@@ -40,14 +42,53 @@ namespace QuantProject.DataAccess.Tables
 					( (DateTime)oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvEndDate" ] !=	Quotes.GetEndDate( ticker ) ) ||
 					( (string)oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvHashValue" ] != hashValue ) )
 					// this ticker was visually already validated, but with different values
-					oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvRangeToRangeRatio" ] = true;
+					oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvRangeToRangeRatio" ] = false;
 				oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvTicker" ] = ticker;
-					oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvStartDate" ] =
+				oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvStartDate" ] =
 					Quotes.GetStartDate( ticker );
 				oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvEndDate" ] =
 					Quotes.GetEndDate( ticker );
 				oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvHashValue" ] = hashValue;
 				oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvCloseToCloseRatio" ] = true;
+				oleDbSingleTableAdapter.OleDbDataAdapter.Update( oleDbSingleTableAdapter.DataTable );
+			}
+			catch ( Exception ex )
+			{
+				string exceptionMessage = ex.Message + "\n" + ex.StackTrace;
+				Console.WriteLine( exceptionMessage );
+			}
+		}
+		/// <summary>
+		/// Writes to the database the visual validation of the Range to Range suspicious ratios
+		/// </summary>
+		/// <param name="ticker">ticker to be validated</param>
+		/// <param name="ticker">quoteDate to be validated</param>
+		public static void ValidateRangeToRange( string ticker , DateTime quoteDate)
+		{
+			try
+			{
+				Quotes quotes = new Quotes( ticker );
+				OleDbSingleTableAdapter oleDbSingleTableAdapter =
+					new OleDbSingleTableAdapter( "select * from visuallyValidatedTickers where vvTicker='"
+						+ ticker + "'" );
+				string hashValue = quotes.GetHashValue(
+					quotes.GetPrecedingDate( quoteDate , ConstantsProvider.PrecedingDaysForVisualValidation ) ,
+					quotes.GetFollowingDate( quoteDate , ConstantsProvider.PrecedingDaysForVisualValidation ) );
+				if ( oleDbSingleTableAdapter.DataTable.Rows.Count == 0 )
+					// this ticker was not visually validated yet
+					oleDbSingleTableAdapter.DataTable.Rows.Add( oleDbSingleTableAdapter.DataTable.NewRow() );
+				else if ( ( (DateTime)oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvEndDate" ] != Quotes.GetEndDate( ticker ) ) ||
+					( (DateTime)oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvEndDate" ] !=	Quotes.GetEndDate( ticker ) ) ||
+					( (string)oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvHashValue" ] != hashValue ) )
+					// this ticker was visually already validated, but with different values
+					oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvCloseToCloseRatio" ] = false;
+				oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvTicker" ] = ticker;
+				oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvStartDate" ] =
+					Quotes.GetStartDate( ticker );
+				oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvEndDate" ] =
+					Quotes.GetEndDate( ticker );
+				oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvHashValue" ] = hashValue;
+				oleDbSingleTableAdapter.DataTable.Rows[ 0 ][ "vvRangeToRangeRatio" ] = true;
 				oleDbSingleTableAdapter.OleDbDataAdapter.Update( oleDbSingleTableAdapter.DataTable );
 			}
 			catch ( Exception ex )
