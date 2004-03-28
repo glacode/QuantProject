616b9294aba9d7b4348fa47c4163fe54c55943e3
glauco_1 <>
2004-03-28 20:06:50 +0000

    Provides constants to be used by the whole application

---
+++ QuantProject/b1_ADT/ConstantsProvider.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace QuantProject.ADT
+{
+	/// <summary>
+	/// Provides constants to be used by the whole application
+	/// </summary>
+	public class ConstantsProvider
+	{
+		public ConstantsProvider()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+		public static int SuspiciousRatio = 3;
+		public static int PrecedingDaysForVisualValidation = 20;
+	}
+}
