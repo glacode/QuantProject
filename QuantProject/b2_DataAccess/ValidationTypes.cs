570367ece7cfc18efeb81f9cc0a0e8f1d1bf4fb3
glauco_1 <>
2004-03-28 20:08:27 +0000

    Types of visual validations

---
+++ QuantProject/b2_DataAccess/ValidationTypes.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace QuantProject.DataAccess
+{
+	/// <summary>
+	/// Types of visual validations
+	/// </summary>
+	public enum ValidationTypes
+	{
+		CloseToCloseRatio = 1 ,
+		RangeToRangeRatio = 2
+	}
+}
