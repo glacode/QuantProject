9b927bbe6d082a8db4a0aecc732b84b4e97d1791
glauco_1 <>
2004-05-09 16:30:21 +0000

    Now it inherits the VisualValidationChart class

--- QuantDownloader/Downloader/QuotesEditor/CloseToClose/CloseToCloseChart.cs
+++ QuantDownloader/Downloader/QuotesEditor/CloseToClose/CloseToCloseChart.cs
@@ -34,53 +34,15 @@ namespace QuantProject.Applications.Downloader
 	/// Displays the charts for the quotes within the interval around the quotes
 	/// with suspicious Close To Close ratio
 	/// </summary>
-	public class CloseToCloseChart : Chart
+	public class CloseToCloseChart : VisualValidationChart
 	{
-    private string ticker;
-    private DateTime suspiciousDateTime;
-    private History history;
-    private DateTime startDateTime;
-    private DateTime endDateTime;
-
-    public string Ticker
-    {
-      get { return this.ticker; }
-      set { this.ticker = value; }
-    }
-    public DateTime SuspiciousDateTime
-    {
-      get { return this.suspiciousDateTime; }
-      set { this.suspiciousDateTime = value; }
-    }
     public CloseToCloseChart()
 		{
-			InitializeComponent();
 		}
-
-    private void InitializeComponent()
-    {
-      this.Name = "CloseToCloseChart";
-    }
-
-    #region "OnPaint"
-    protected void onPaint_setTimeInterval()
-    {
-      this.startDateTime = (DateTime) this.history.GetKey( Math.Max( 0 ,
-        this.history.IndexOfKeyOrPrevious( this.suspiciousDateTime ) -
-				ConstantsProvider.PrecedingDaysForVisualValidation ) );
-      this.endDateTime = (DateTime) this.history.GetKey( Math.Min( this.history.Count - 1 ,
-        this.history.IndexOfKeyOrPrevious( this.suspiciousDateTime ) ) +
-				ConstantsProvider.PrecedingDaysForVisualValidation );
-    }
-    protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
-    {
-      Console.WriteLine( "CloseToCloseChart.OnPaint()" );
-      this.Clear();
-      this.history = DataProvider.GetCloseHistory( ((QuotesEditor)this.FindForm()).Ticker );
-      this.onPaint_setTimeInterval();
-      this.Add( this.history , Color.Red , this.startDateTime , this.endDateTime );
-      base.OnPaint( e );
-    }
-    #endregion
+		protected override void addHistories()
+		{
+			History lowHistory = DataProvider.GetCloseHistory( ((QuotesEditor)this.FindForm()).Ticker );
+			this.add( lowHistory , Color.Green );
+		}
   }
 }
