d63141a3f1d1f39f79e8e9933ae5f84be9aaa8b8
glauco_1 <>
2004-05-26 15:38:50 +0000

    The constructor has been overloaded and the SetAdapter method has been added.

--- QuantProject/b2_DataAccess/OleDbSingleTableAdapter.cs
+++ QuantProject/b2_DataAccess/OleDbSingleTableAdapter.cs
@@ -1,3 +1,25 @@
+/*
+QuantProject - Quantitative Finance Library
+
+OleDbSingleTableAdapter.cs
+Copyright (C) 2003 
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 using System;
 using System.Data;
 using System.Data.OleDb;
@@ -27,20 +49,17 @@ namespace QuantProject.DataAccess
 		{
 			get { return this.dataTable; }
 		}
-		
-		public OleDbSingleTableAdapter( string selectStatement )
+
+		private void setAdapter( string selectStatement )
 		{
 			try
 			{
-//				string selectStatement =
-//					"select * from " + tableName + " where 1=2";
 				this.oleDbDataAdapter =
 					new OleDbDataAdapter( selectStatement , ConnectionProvider.OleDbConnection );
 				OleDbCommandBuilder oleDbCommandBuilder = new OleDbCommandBuilder( oleDbDataAdapter );
 				this.oleDbDataAdapter.InsertCommand = oleDbCommandBuilder.GetInsertCommand();
 				this.oleDbDataAdapter.UpdateCommand = oleDbCommandBuilder.GetUpdateCommand();
 				this.oleDbDataAdapter.DeleteCommand = oleDbCommandBuilder.GetDeleteCommand();
-				this.dataTable = new DataTable();
 				this.oleDbDataAdapter.Fill( this.dataTable );
 			}
 			catch ( Exception ex )
@@ -49,33 +68,28 @@ namespace QuantProject.DataAccess
 				Console.WriteLine( exceptionMessage );
 			}
 		}
+
+		public OleDbSingleTableAdapter( string selectStatement )
+		{
+			this.dataTable = new DataTable();
+			setAdapter( selectStatement );
+		}
     
     public OleDbSingleTableAdapter(string selectStatement, DataTable table )
     {
-      try
-      {
-        
-        //				string selectStatement =
-        //					"select * from " + tableName + " where 1=2";
-        this.oleDbDataAdapter =
-          new OleDbDataAdapter( selectStatement , ConnectionProvider.OleDbConnection );
-        OleDbCommandBuilder oleDbCommandBuilder = new OleDbCommandBuilder( oleDbDataAdapter );
-        this.oleDbDataAdapter.InsertCommand = oleDbCommandBuilder.GetInsertCommand();
-        this.oleDbDataAdapter.UpdateCommand = oleDbCommandBuilder.GetUpdateCommand();
-        this.oleDbDataAdapter.DeleteCommand = oleDbCommandBuilder.GetDeleteCommand();
-        this.dataTable = table;
-        this.oleDbDataAdapter.Fill( this.dataTable );
-      }
-      catch ( Exception ex )
-      {
-        string exceptionMessage = ex.Message + "\n" + ex.StackTrace;
-        Console.WriteLine( exceptionMessage );
-      }
+			this.dataTable = table;
+			setAdapter( selectStatement );
     }
 
+		public OleDbSingleTableAdapter()
+		{
+		}
 
-//		public void SetAdapterAndDataTable( string tableName )
-//		{
-//		}
+		public void SetAdapter( string tableName )
+		{
+			string selectStatement =
+				"select * from " + tableName + " where 1=2";
+			setAdapter( selectStatement );
+		}
 	}
 }
