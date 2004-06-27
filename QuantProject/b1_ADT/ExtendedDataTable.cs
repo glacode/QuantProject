bc90a814d32354c2b11c26d6304aaba12b6820a2
millo_1 <>
2004-06-27 19:15:02 +0000

    Added Sort and Delete static methods to the class

--- QuantProject/b1_ADT/ExtendedDataTable.cs
+++ QuantProject/b1_ADT/ExtendedDataTable.cs
@@ -14,5 +14,38 @@ namespace QuantProject.ADT
 			// TODO: Add constructor logic here
 			//
 		}
+    
+    /// <summary>
+    /// Sort the given DataTable by the specified field, in a DESC mode
+    /// </summary>
+    
+    public static void Sort(DataTable tableToSort, string sortingFieldName)
+    {
+      DataTable copyOfTableToSort = tableToSort.Copy();
+      DataRow[] orderedRows = copyOfTableToSort.Select("", sortingFieldName + " DESC");
+      int numRows = tableToSort.Rows.Count;
+      int numColumns = tableToSort.Columns.Count;
+      object[] valuesToAdd = new object[numColumns];
+      tableToSort.Rows.Clear();
+      for(int i = 0;i<numRows;i++)
+      {
+        for(int j = 0;j<numColumns;j++)
+        {
+          valuesToAdd[j]=orderedRows[i][j];
+        }
+        tableToSort.Rows.Add(valuesToAdd);
+      }
+      tableToSort.AcceptChanges();
+      
+    }
+    public static void DeleteRows(DataTable tableWithRowsToDelete, long indexOfRowFromWhichDeletionHasToBeDone)
+    {
+      for(long i = indexOfRowFromWhichDeletionHasToBeDone;i<tableWithRowsToDelete.Rows.Count;i++)
+      {
+        tableWithRowsToDelete.Rows.RemoveAt((int)i);
+      }
+      tableWithRowsToDelete.AcceptChanges();
+    }
+
 	}
 }
