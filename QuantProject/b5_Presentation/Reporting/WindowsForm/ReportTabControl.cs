5fcc8d6c43244933f89c459fb2fd6db1d3b6ad06
glauco_1 <>
2004-08-01 00:18:48 +0000

    TabControl for the report form

---
+++ QuantProject/b5_Presentation/Reporting/WindowsForm/ReportTabControl.cs
@@ -0,0 +1,47 @@
+/*
+QuantProject - Quantitative Finance Library
+
+ReportTabControl.cs
+Copyright (C) 2003 
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+using System;
+using System.Windows.Forms;
+using QuantProject.Business.Financial.Accounting.Reporting;
+
+namespace QuantProject.Presentation.Reporting.WindowsForm
+{
+	/// <summary>
+	/// TabControl for the report form
+	/// </summary>
+	public class ReportTabControl : TabControl
+	{
+		private AccountReport accountReport;
+		private TabPage equityChart;
+		private TabPage summary;
+
+		public ReportTabControl( AccountReport accountReport )
+		{
+			this.accountReport = accountReport;
+			this.Dock = DockStyle.Fill;
+			this.equityChart = new EquityChartTabPage( this.accountReport );
+			this.Controls.Add( this.equityChart );
+			this.summary = new SummaryTabPage( this.accountReport );
+			this.Controls.Add( this.summary );
+		}
+	}
+}
