93a6486b1e7b1203ae047e36a559f197f0d85326
glauco_1 <>
2004-08-08 13:45:44 +0000

    Report TabPage, to contain a ReportGrid

--- QuantProject/b5_Presentation/Reporting/WindowsForm/ReportGridTabPage.cs
+++ QuantProject/b5_Presentation/Reporting/WindowsForm/ReportGridTabPage.cs
@@ -1,3 +1,24 @@
+/*
+QuantProject - Quantitative Finance Library
+
+ReportTabControl.cs
+Copyright (C) 2003 
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 using System;
 using System.Windows.Forms;
 using QuantProject.Business.Financial.Accounting.Reporting;
@@ -5,7 +26,7 @@ using QuantProject.Business.Financial.Accounting.Reporting;
 namespace QuantProject.Presentation.Reporting.WindowsForm
 {
 	/// <summary>
-	/// Summary description for RoundTradesTabPage.
+	/// Report TabPage, to contain a ReportGrid
 	/// </summary>
 	public class ReportGridTabPage : TabPage
 	{
