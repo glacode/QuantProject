dfc6bf85e79d7314c21e351dff3580d8325f7dba
glauco_1 <>
2004-08-28 16:58:13 +0000

    EventArgs for the NewQuote event

---
+++ QuantProject/b3_Data/DataProviders/NewQuoteEventArgs.cs
@@ -0,0 +1,48 @@
+/*
+QuantProject - Quantitative Finance Library
+
+NewQuoteEventArgs.cs
+Copyright (C) 2003 
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+using System;
+using System.Data;
+
+namespace QuantProject.Data.DataProviders
+{
+	/// <summary>
+	/// EventArgs for the NewQuote event
+	/// </summary>
+	public class NewQuoteEventArgs : EventArgs
+	{
+    private Quote quote;
+
+    /// <summary>
+    /// The new quote
+    /// </summary>
+    public Quote Quote
+    {
+      get { return this.quote; }
+      set { this.quote = value; }
+    }
+
+    public NewQuoteEventArgs( Quote quote )
+    {
+      this.quote = quote;
+    }
+  }
+}
