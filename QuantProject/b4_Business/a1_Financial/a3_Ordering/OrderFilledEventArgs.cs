f6f04b3f4039baf22107842025484ae820c56675
glauco_1 <>
2004-11-29 15:06:29 +0000

    EventArgs for the OrderExecuted event

---
+++ QuantProject/b4_Business/a1_Financial/a3_Ordering/OrderFilledEventArgs.cs
@@ -0,0 +1,58 @@
+/*
+QuantProject - Quantitative Finance Library
+
+NewTransactionEventArgs.cs
+Copyright (C) 2003 
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+using System;
+using System.Data;
+
+using QuantProject.ADT;
+using QuantProject.Business.Financial.Ordering;
+using QuantProject.Business.Financial.Accounting;
+using QuantProject.Business.Financial.Accounting.Transactions;
+
+namespace QuantProject.Business.Financial.Ordering
+{
+	/// <summary>
+	/// EventArgs for the OrderExecuted event
+	/// </summary>
+	public class OrderFilledEventArgs : EventArgs
+	{
+		private Order order;
+		private EndOfDayTransaction endOfDayTransaction;
+
+		public Order Order
+		{
+			get { return this.order; }
+			set { this.order = value; }
+		}
+
+		public EndOfDayTransaction EndOfDayTransaction
+		{
+			get { return this.endOfDayTransaction; }
+			set { this.endOfDayTransaction = value; }
+		}
+
+    public OrderFilledEventArgs( Order order , EndOfDayTransaction endOfDayTransaction )
+    {
+      this.order = order;
+			this.endOfDayTransaction = endOfDayTransaction;
+    }
+  }
+}
