270c1ad58c3f5e98774f2b7090f2a038dc9ed619
glauco_1 <>
2004-11-29 15:22:31 +0000

    - implemented the interface IComparable - added the GetFitnessValue method - added an IEndOfDayTimer - added an IDataStreamer - added an IOrderExecutor - added an ArrayList of active orders - added an AccountReport - added the AddOrder method - added Contains( Instrument instrument ) - added Contains( string ticker ) - added Add( EndOfDayTransaction transaction ) - added GetMarketValue( EndOfDayDateTime endOfDayDateTime ) instead of ExtendedDateTime parameter - added GetMarketValue( string ticker ) - added GetProfitNetLoss( EndOfDayDateTime endOfDayDateTime ) instead of ExtendedDateTime parameter - CreateReport now uses an EndOfDayDateTime parameter instead of an ExtendedDateTime parameter

--- QuantProject/b4_Business/a1_Financial/a2_Accounting/Account.cs
+++ QuantProject/b4_Business/a1_Financial/a2_Accounting/Account.cs
@@ -30,10 +30,14 @@ using System.Runtime.Serialization.Formatters.Soap;
 using Excel;
 using QuantProject.ADT;
 using QuantProject.ADT.Histories;
-//using QuantProject.Data.MicrosoftExcel;
+using QuantProject.Data.DataProviders;
 using QuantProject.Business.Financial.Accounting.Reporting;
+using QuantProject.Business.Financial.Accounting.Transactions;
 using QuantProject.Business.Financial.Instruments;
+using QuantProject.Business.Financial.Ordering;
 using QuantProject.Business.Strategies;
+using QuantProject.Business.Timing;
+
 
 namespace QuantProject.Business.Financial.Accounting
 {
@@ -43,29 +47,49 @@ namespace QuantProject.Business.Financial.Accounting
   /// 
 
   [Serializable]
-  public class Account : Keyed
+  public class Account : Keyed , IComparable
   {
     private double cashAmount;
     private AccountStrategy accountStrategy;
+		private IEndOfDayTimer endOfDayTimer;
+		private IDataStreamer dataStreamer;
+		private IOrderExecutor orderExecutor;
+		private ArrayList activeOrders;
+		private AccountReport accountReport;
 
     public Portfolio Portfolio = new Portfolio();
     //public AccountReport accountReport;
 
-    public double CashAmount
-    {
-      get
-      {
-        return cashAmount;
-      }
-    }
+		public double CashAmount
+		{
+			get	{	return cashAmount; }
+		}
 
-    public AccountStrategy AccountStrategy
+		public IEndOfDayTimer EndOfDayTimer
+		{
+			get	{	return this.endOfDayTimer;	}
+			set	{	this.endOfDayTimer = value;	}
+		}
+
+		public IDataStreamer DataStreamer
+		{
+			get	{	return this.dataStreamer;	}
+			set	{	this.dataStreamer = value;	}
+		}
+
+		public IOrderExecutor OrderExecutor
+		{
+			get	{	return this.orderExecutor;	}
+			set	{	this.orderExecutor = value;	}
+		}
+
+		public AccountStrategy AccountStrategy
     {
       get { return accountStrategy; }
       set { accountStrategy = value; }
     }
 
-    public Transactions Transactions = new Transactions();
+    public TransactionHistory Transactions = new TransactionHistory();
 
     public Account( string accountName ) : base ( accountName )
     {
@@ -75,6 +99,7 @@ namespace QuantProject.Business.Financial.Accounting
     private void initialize()
     {
       cashAmount = 0;
+			this.activeOrders = new ArrayList();
       accountStrategy = new AccountStrategy( this );
     }
 
@@ -82,7 +107,40 @@ namespace QuantProject.Business.Financial.Accounting
     {
       this.initialize();
     }
+		public Account( string accountName , IEndOfDayTimer endOfDayTimer ,
+			IDataStreamer dataStreamer , IOrderExecutor orderExecutor ) : base( accountName )
+		{
+			this.endOfDayTimer = endOfDayTimer;
+			this.dataStreamer = dataStreamer;
+			this.orderExecutor = orderExecutor;
+			this.orderExecutor.OrderFilled += new OrderFilledEventHandler(
+				this.orderFilledEventHandler );
+			this.initialize();
+		}
 
+		private void orderFilledEventHandler( Object sender , OrderFilledEventArgs
+			eventArgs )
+		{
+			this.Add( eventArgs.EndOfDayTransaction );
+		}
+
+		public virtual double GetFitnessValue()
+		{
+			if ( this.accountReport == null )
+				// the account report has not been computed yet
+				this.accountReport = this.CreateReport( this.Key ,
+					1 , this.endOfDayTimer.GetCurrentTime() );
+			return this.accountReport.Summary.ReturnOnAccount;
+		}
+		public int CompareTo( Object account )
+		{
+			int returnValue = 0;
+			if ( this.GetFitnessValue() < (( Account )account).GetFitnessValue() )
+				returnValue = -1;
+			if ( this.GetFitnessValue() > (( Account )account).GetFitnessValue() )
+				returnValue = 1;
+			return returnValue;
+		}
     public void Clear()
     {
       this.cashAmount = 0;
@@ -106,12 +164,25 @@ namespace QuantProject.Business.Financial.Accounting
       }
     }
 
-    public bool Contains( Instrument instrument )
-    {
-      return Portfolio.Contains( instrument );
-    }
+		public void AddCash( double moneyAmount )
+		{
+			this.AddCash( this.endOfDayTimer.GetCurrentTime().GetNearestExtendedDateTime() ,
+				moneyAmount );
+		}
 
-    #region "Add( TimedTransaction transaction )"
+		public void AddOrder( Order order )
+		{
+			this.orderExecutor.Execute( order );
+		}
+		public bool Contains( Instrument instrument )
+		{
+			return Portfolio.Contains( instrument );
+		}
+		public bool Contains( string ticker )
+		{
+			return Portfolio.Contains( ticker );
+		}
+
 
     private void updateCash( Transaction transaction )
     {
@@ -125,29 +196,41 @@ namespace QuantProject.Business.Financial.Accounting
       this.Portfolio.Update( transaction );
       //this.accountReport.AddRecord( this );
     }
-    #endregion
+		public void Add( EndOfDayTransaction transaction )
+		{
+			this.Transactions.Add( transaction );
+			this.updateCash( transaction );
+			this.Portfolio.Update( transaction );
+			//this.accountReport.AddRecord( this );
+		}
 
-    public double GetMarketValue( ExtendedDateTime extendedDateTime )
+
+		public double GetMarketValue( EndOfDayDateTime endOfDayDateTime )
+		{
+			return this.CashAmount + this.Portfolio.GetMarketValue( endOfDayDateTime );
+		}
+		public double GetMarketValue( string ticker )
+		{
+			return HistoricalDataProvider.GetMarketValue( ticker ,
+				this.endOfDayTimer.GetCurrentTime().GetNearestExtendedDateTime() );
+		}
+		public double GetProfitNetLoss( EndOfDayDateTime endOfDayDateTime )
     {
-      return this.CashAmount + this.Portfolio.GetMarketValue( extendedDateTime );
-    }
-    public double GetProfitNetLoss( ExtendedDateTime extendedDateTime )
-    {
-      return GetMarketValue( extendedDateTime ) +
+      return GetMarketValue( endOfDayDateTime ) +
         this.Transactions.TotalWithdrawn -
         this.Transactions.TotalAddedCash;
     }
 
-    public History GetProfitNetLossHistory( ExtendedDateTime finalDateTime )
+    public History GetProfitNetLossHistory( EndOfDayDateTime finalDateTime )
     {
       History history = new History();
       Account account = new Account( "ToGetProfitNetLossHistory" );
       foreach ( ArrayList arrayList in this.Transactions.Values )
-        foreach ( TimedTransaction transaction in arrayList )
+        foreach ( EndOfDayTransaction transaction in arrayList )
         {
           account.Add( transaction );
-          history.MultiAdd( transaction.ExtendedDateTime.DateTime ,
-            account.GetProfitNetLoss( transaction.ExtendedDateTime ) );
+          history.MultiAdd( transaction.EndOfDayDateTime.DateTime ,
+            account.GetProfitNetLoss( transaction.EndOfDayDateTime ) );
         }
       history.MultiAdd( finalDateTime.DateTime ,
         account.GetProfitNetLoss( finalDateTime ) );
@@ -160,33 +243,56 @@ namespace QuantProject.Business.Financial.Accounting
         "\nCashAmount : " + this.CashAmount +
         "\nPortfolioContent : " + this.Portfolio.ToString() +
         "\nPortfolioMarketValue : " + this.Portfolio.GetMarketValue(
-          new ExtendedDateTime( dateTime , BarComponent.Close ) ) +
+          new EndOfDayDateTime( dateTime , EndOfDaySpecificTime.MarketClose ) ) +
         "\nAccountProfitNetLoss : " + this.GetProfitNetLoss(
-          new ExtendedDateTime( dateTime , BarComponent.Close ) );
+          new EndOfDayDateTime( dateTime , EndOfDaySpecificTime.MarketClose ) );
     }
 
-    public AccountReport CreateReport( string reportName ,
-      int numDaysForInterval , ExtendedDateTime endDateTime )
-    {
-      AccountReport accountReport = new AccountReport( this );
-      return accountReport.Create( reportName , numDaysForInterval , endDateTime );
-    }
-    public AccountReport CreateReport( string reportName ,
-      int numDaysForInterval , ExtendedDateTime endDateTime , string buyAndHoldTicker )
-    {
-      AccountReport accountReport = new AccountReport( this );
-      return accountReport.Create( reportName , numDaysForInterval ,
-        endDateTime , buyAndHoldTicker );
-    }
-    public void Serialize( string filePathAndName )
-    {
-//      //Dim FS As New IO.FileStream("c:\Rect.xml", IO.FileMode.Create, IO.FileAccess.Write)
-//   Dim XMLFormatter As New SoapFormatter()
-//   Dim R As New Rectangle(8, 8, 299, 499)
-//   XMLFormatter.Serialize(FS, R)
-      FileStream fileStream = new FileStream( filePathAndName , FileMode.Create , FileAccess.Write );
-      SoapFormatter soapFormatter = new SoapFormatter();
-      soapFormatter.Serialize( fileStream , this );
-    }
-  }
+		public AccountReport CreateReport( string reportName ,
+			int numDaysForInterval , EndOfDayDateTime endDateTime )
+		{
+			AccountReport accountReport = new AccountReport( this );
+			return accountReport.Create( reportName , numDaysForInterval , endDateTime );
+		}
+		public AccountReport CreateReport( string reportName ,
+			int numDaysForInterval , EndOfDayDateTime endDateTime , string buyAndHoldTicker )
+		{
+			AccountReport accountReport = new AccountReport( this );
+			return accountReport.Create( reportName , numDaysForInterval ,
+				endDateTime , buyAndHoldTicker );
+		}
+		public void Serialize( string filePathAndName )
+		{
+			//      //Dim FS As New IO.FileStream("c:\Rect.xml", IO.FileMode.Create, IO.FileAccess.Write)
+			//   Dim XMLFormatter As New SoapFormatter()
+			//   Dim R As New Rectangle(8, 8, 299, 499)
+			//   XMLFormatter.Serialize(FS, R)
+			FileStream fileStream = new FileStream( filePathAndName , FileMode.Create , FileAccess.Write );
+			SoapFormatter soapFormatter = new SoapFormatter();
+			soapFormatter.Serialize( fileStream , this );
+		}
+		#region ClosePosition_position
+		private OrderType closePosition_getOrderType( Position position )
+		{
+			OrderType returnValue;
+			if ( position.Quantity >= 0 )
+				// long position
+				returnValue = OrderType.MarketSell;
+			else
+				// short position
+				returnValue = OrderType.MarketCover;
+			return returnValue;
+		}
+		public void ClosePosition( Position position )
+		{
+			OrderType orderType = closePosition_getOrderType( position );
+			Order order = new Order( orderType , position.Instrument , position.Quantity );
+			this.orderExecutor.Execute( order );
+		}
+		#endregion ClosePosition_position
+		public void ClosePosition( string ticker )
+		{
+			this.ClosePosition( (Position)this.Portfolio[ ticker ] );
+		}
+	}
 }
