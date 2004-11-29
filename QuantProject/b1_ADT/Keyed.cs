3e83a13cb8891d7b46762f0745fc8ac11b89b880
glauco_1 <>
2004-11-29 16:11:34 +0000

    Indentation changes

--- QuantProject/b1_ADT/Keyed.cs
+++ QuantProject/b1_ADT/Keyed.cs
@@ -27,33 +27,33 @@ namespace QuantProject.ADT
 	/// <summary>
 	/// Summary description for Keyed.
 	/// </summary>
-  [Serializable]
-  public class Keyed
-  {
-    private string key;
+	[Serializable]
+	public class Keyed
+	{
+		private string key;
     
-    public string Key
-    {
-      get
-      {
-        return key;
-      }
-      set
-      {
-        key = value;
-      }
-    }
+		public string Key
+		{
+			get
+			{
+				return key;
+			}
+			set
+			{
+				key = value;
+			}
+		}
 
-    public Keyed()
-    {
-      //
-      // TODO: Add constructor logic here
-      //
-    }
+		public Keyed()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
   
-    public Keyed( string key )
-    {
-      Key = key;
-    }
-  }
+		public Keyed( string key )
+		{
+			Key = key;
+		}
+	}
 }
