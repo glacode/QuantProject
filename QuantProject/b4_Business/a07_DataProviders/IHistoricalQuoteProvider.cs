ff99dbe20017898bd5f4866d731dd689617ae866
glauco_1 <>
2005-01-09 23:29:41 +0000

    Interface to be implemented by historical quote providers

---
+++ QuantProject/b4_Business/a07_DataProviders/IHistoricalQuoteProvider.cs
@@ -0,0 +1,45 @@
+/*
+QuantProject - Quantitative Finance Library
+
+IHistoricalQuoteProvider.cs
+Copyright (C) 2003 
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+using System;
+using System.Data;
+using QuantProject.ADT;
+using QuantProject.ADT.Histories;
+using QuantProject.Data.DataTables;
+using QuantProject.Business.Timing;
+
+namespace QuantProject.Business.DataProviders
+{
+	/// <summary>
+	/// Interface to be implemented by historical quote providers
+	/// </summary>
+	public interface IHistoricalQuoteProvider
+	{
+		/// <summary>
+		/// Returns the historical market value for the given instrument at the given time
+		/// </summary>
+		/// <param name="instrumentKey">instrument identifier</param>
+		/// <param name="endOfDayDateTime">end of day date time for the market evaluation</param>
+		/// <returns></returns>
+    double GetMarketValue( string instrumentKey , EndOfDayDateTime endOfDayDateTime );
+	}
+}
