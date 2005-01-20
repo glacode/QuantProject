895812fc0a5737d408160c2225b8910f719ee019
glauco_1 <>
2005-01-20 01:25:54 +0000

    The benchmark buy and hold equity line has been added to the report

--- QuantProject/b5_Presentation/Reporting/WindowsForm/EquityChartTabPage.cs
+++ QuantProject/b5_Presentation/Reporting/WindowsForm/EquityChartTabPage.cs
@@ -20,9 +20,13 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 using QuantProject.ADT.Histories;
+using QuantProject.Data.DataProviders;
+using QuantProject.Business.DataProviders;
 using QuantProject.Business.Financial.Accounting.Reporting;
+using QuantProject.Business.Timing;
 using QuantProject.Presentation.Charting;
 
 namespace QuantProject.Presentation.Reporting.WindowsForm
@@ -34,18 +38,42 @@ namespace QuantProject.Presentation.Reporting.WindowsForm
 	{
 		private AccountReport accountReport;
 		private Chart equityChart;
-
+		private History equity = new History();
+		private History benchmark;
+		
+		/// <summary>
+		/// Returns the history for the BuyAndHoldTicker, normalized for
+		/// a proper display, that is, to be displayed overlapped to
+		/// the equity line
+		/// </summary>
+		/// <returns></returns>
+		private History getBenchmark()
+		{
+			History buyAndHoldTickerEquityLine =
+				HistoricalDataProvider.GetAdjustedCloseHistory(
+				this.accountReport.BuyAndHoldTicker );
+			HistoricalAdjustedQuoteProvider quoteProvider =
+				new HistoricalAdjustedQuoteProvider();
+			DateTime firstDate = (DateTime)this.equity.GetKey( 0 );
+			double normalizingFactor =
+				( double )this.equity[ firstDate ] /
+				( double )quoteProvider.GetMarketValue( this.accountReport.BuyAndHoldTicker ,
+				new EndOfDayDateTime( firstDate , EndOfDaySpecificTime.MarketClose ) );
+			return buyAndHoldTickerEquityLine.MultiplyBy( normalizingFactor );
+		}
 		public EquityChartTabPage( AccountReport accountReport )
 		{
 			this.Text = "Equity Line";
 			this.accountReport = accountReport;
 			this.equityChart = new Chart();
 			this.equityChart.Dock = DockStyle.Fill;
-			History historyToBeCharted = new History();
-			historyToBeCharted.Import( this.accountReport.Equity.DataTable ,
+			this.equity.Import( this.accountReport.Equity.DataTable ,
 				QuantProject.Business.Financial.Accounting.Reporting.Tables.Equity.Date ,
 				QuantProject.Business.Financial.Accounting.Reporting.Tables.Equity.AccountValue );
-			this.equityChart.Add( historyToBeCharted );
+			this.equityChart.Add( equity , Color.Red );
+			this.benchmark = this.getBenchmark();
+			this.equityChart.Add( benchmark , Color.Blue , (DateTime)this.equity.GetKey( 0 ) ,
+				(DateTime)this.equity.GetKey( this.equity.Count - 1 ) );
 			this.Controls.Add( this.equityChart );
 		}
 //		protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
