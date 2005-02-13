85baf40c628db5d4e6a275501c21221ab8bc57bc
glauco_1 <>
2005-02-13 21:00:49 +0000

    Account report Summary Row, containing the total commission amount.

---
+++ QuantProject/b4_Business/a1_Financial/a2_Accounting/h5_Reporting/SummaryRows/TotalCommissionAmount.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Data;
+using QuantProject.ADT;
+using QuantProject.Business.Financial.Accounting.Reporting.Tables;
+using QuantProject.Business.Financial.Instruments;
+
+namespace QuantProject.Business.Financial.Accounting.Reporting.SummaryRows
+{
+	/// <summary>
+	/// Account report Summary Row, containing the total
+	/// commission amount.
+	/// </summary>
+	public class TotalCommissionAmount : SummaryRow
+	{
+		public TotalCommissionAmount( Summary summary )
+		{
+      this.rowDescription = "Total commission amount";
+      double avgReturn =
+        (double) summary.AccountReport.TransactionTable.DataTable.Compute( "sum([Commission])" , "true" );
+      this.rowValue = avgReturn;
+    }
+	}
+}
