e77d9b03c18e231fea9fc638eb18e67f6ba3782d
millo_1 <>
2005-03-28 18:11:42 +0000

    Added new SelectionType, for the new ITickerSelector object: SelectorByAverageRawOpenPrice has been added

--- QuantProject/b3_Data/Selectors/SelectionType.cs
+++ QuantProject/b3_Data/Selectors/SelectionType.cs
@@ -38,6 +38,7 @@ namespace QuantProject.Data.Selectors
     CloseToOpenVolatility,
     CloseToOpenLinearCorrelation,
     CloseToCloseLinearCorrelation,
-    QuotedAtEachMarketDay
+    QuotedAtEachMarketDay,
+    AverageRawOpenPrice
   }
 }
