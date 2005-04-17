daa343346a6aba4533334a97ef82fe0f73ef4bbe
glauco_1 <>
2005-04-17 22:11:22 +0000

    Account report rummary rows that compute the Equity Line vs Benchmark comparison

---
+++ QuantProject/b4_Business/a1_Financial/a2_Accounting/h5_Reporting/SummaryRows/NumberWinningPeriods.cs
@@ -0,0 +1,76 @@
+/*
+QuantProject - Quantitative Finance Library
+
+NumberWinningPeriods.cs
+Copyright (C) 2003 
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+using System;
+using System.Data;
+using QuantProject.ADT;
+using QuantProject.Business.Financial.Accounting.Reporting.Tables;
+using QuantProject.Business.Financial.Instruments;
+
+namespace QuantProject.Business.Financial.Accounting.Reporting.SummaryRows
+{
+	/// <summary>
+	/// Summary row that computes the Equity Line vs Benchmark comparison
+	/// </summary>
+	[Serializable]
+	public class NumberWinningPeriods : SummaryRow
+	{
+		private Summary summary;
+
+		private void setWinningLosingAndEvenPeriods_forPeriod( int i )
+		{
+			double equityHistoryGain =
+				( (double)this.summary.AccountReport.EquityHistory.GetByIndex( i + 1 ) -
+				(double)this.summary.AccountReport.EquityHistory.GetByIndex( i ) ) /
+				(double)this.summary.AccountReport.EquityHistory.GetByIndex( i );
+			double benchmarkGain =
+				( (double)this.summary.AccountReport.BenchmarkEquityLine.GetByIndex( i + 1 ) -
+				(double)this.summary.AccountReport.BenchmarkEquityLine.GetByIndex( i ) ) /
+				(double)this.summary.AccountReport.BenchmarkEquityLine.GetByIndex( i );
+			if ( ( equityHistoryGain - benchmarkGain ) > ConstantsProvider.MinForDifferentGains )
+				this.summary.NumberWinningPeriods++;
+			else
+			{
+				if ( ( benchmarkGain - equityHistoryGain ) > ConstantsProvider.MinForDifferentGains )
+					this.summary.NumberLosingPeriods++;
+				else
+					this.summary.NumberEvenPeriods++;
+			}
+		}
+
+		public void SetWinningLosingAndEvenPeriods()
+		{
+			this.summary.NumberWinningPeriods = 0;
+			this.summary.NumberLosingPeriods = 0;
+			this.summary.NumberEvenPeriods = 0;
+			for ( int i=0; i<this.summary.AccountReport.EquityHistory.Count - 1 ; i++ )
+				this.setWinningLosingAndEvenPeriods_forPeriod( i );
+		}
+		public NumberWinningPeriods( Summary summary )
+		{
+			this.summary = summary;
+			this.SetWinningLosingAndEvenPeriods();
+			this.rowDescription = "# winning periods";
+			this.rowValue = this.summary.NumberWinningPeriods;
+		}
+	}
+}
---
+++ QuantProject/b4_Business/a1_Financial/a2_Accounting/h5_Reporting/SummaryRows/PercentageWinningPeriods.cs
@@ -0,0 +1,43 @@
+/*
+QuantProject - Quantitative Finance Library
+
+PercentageWinningPeriods.cs
+Copyright (C) 2003 
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+using System;
+using System.Data;
+using QuantProject.ADT;
+using QuantProject.Business.Financial.Accounting.Reporting.Tables;
+using QuantProject.Business.Financial.Instruments;
+
+namespace QuantProject.Business.Financial.Accounting.Reporting.SummaryRows
+{
+	/// <summary>
+	/// Summary row that computes the Equity Line vs Benchmark comparison
+	/// </summary>
+	[Serializable]
+	public class PercentageWinningPeriods : SummaryRow
+	{
+		public PercentageWinningPeriods( Summary summary )
+		{
+			this.rowDescription = "# losing periods";
+			this.rowValue = summary.PercentageWinningPeriods;
+		}
+	}
+}
