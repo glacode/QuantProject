177264be460d21775bfc486420d9c98085a75ab2
glauco_1 <>
2005-05-26 22:54:38 +0000

    Exception thrown when the key to find is less than the first object

---
+++ QuantProject/b1_ADT/IndexOfKeyOrPreviousException.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Windows.Forms;
+
+namespace QuantProject.ADT
+{
+	/// <summary>
+	/// Exception thrown when the key to find is less than the first object
+	/// </summary>
+	public class IndexOfKeyOrPreviousException : Exception
+	{
+		public IndexOfKeyOrPreviousException(
+			IComparable firstKeyInTheAdvancedSortedList , Object key )
+		{
+			string message = "The given key is less than the first object " +
+				"in the AdvancedSortedList";
+			message = message;
+//			MessageBox.Show( this.message );
+		}
+	}
+}
