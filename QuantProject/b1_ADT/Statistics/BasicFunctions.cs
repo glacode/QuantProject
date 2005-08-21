0b2d64022c2c814fa3c687fbba631dcefcd4f5bd
millo_1 <>
2005-08-21 10:09:23 +0000

    Added statistic methods for getting Percentile, Median and DecileAverage (even though these methods are not used by the project, at the moment).

--- QuantProject/b1_ADT/Statistics/BasicFunctions.cs
+++ QuantProject/b1_ADT/Statistics/BasicFunctions.cs
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 using System;
+using QuantProject.ADT.Statistics;
 
 namespace QuantProject.ADT.Statistics
 {
@@ -29,7 +30,16 @@ namespace QuantProject.ADT.Statistics
 	/// </summary>
 	public class BasicFunctions
 	{
-    static public  double Sum( double[] data ) 
+   	private static double[] orderedData;
+    
+    static private void setOrderedData(double[] data)
+    {
+  	 	orderedData = new double[data.Length];
+      data.CopyTo(orderedData,0);
+      Array.Sort(orderedData);
+    }
+		
+		static public  double Sum( double[] data )
     {
       double sum = 0;
       for( int i = 0; i < data.Length ; i ++ ) 
@@ -184,6 +194,82 @@ namespace QuantProject.ADT.Statistics
       if(firstDataVariable.Length !=secondDataVariable.Length)
         throw new Exception("The two variables haven't the same length!");
     }
+		
+		/// <summary>
+    /// Returns the t-th percentile for a given set of data
+    /// </summary>
+    /// <param name="data">Data for which t-th percentile is to be computed </param>
+    /// <param name="percentileNumber">A number between 0 and 100, corresponding
+    ///  to the requested t-th percentile	</param>
+    static public double Percentile(double[] data,
+                                    int percentileNumber)
+    {
+    	if(percentileNumber<0 || percentileNumber >100)
+    		throw new Exception("percentileNumber has to be a number between 0 and 100!");
+    	setOrderedData(data);
+    	int n = orderedData.Length;
+    	double np = ((double)n+1) * (double)percentileNumber/100.0;
+      if(np<=1.0)
+        return orderedData[0];
+      if(np>=n)
+        return orderedData[n-1];
+      
+      int integerPart_np = (int)Math.Floor(np);
+      double fractionalPart_np = np - integerPart_np;
+      //if np is not a whole number, it has to be returned a
+      // number between two data points
+      // In this implementation the method used to return such number
+      // is based on a simple interpolation 
+      return (orderedData[integerPart_np-1] +
+              fractionalPart_np* (orderedData[integerPart_np] - orderedData[integerPart_np-1]));
+     
+    }
+	    
+    /// <summary>
+    /// Returns the median for a given set of  data
+    /// (Median is the value, belonging or not to the given set of data, such that
+    /// number of data points below that value is equal to the number of data points
+    /// above that value)
+    /// </summary>
+    /// <param name="data">Data for which median is to be computed</param>
+    static public double Median(double[] data)
+    {
+    	return Percentile(data, 50);
+    }
+    
+    /// <summary>
+    /// Returns the average value for the given decile of
+    /// a given set of data
+    /// </summary>
+    /// <param name="data">The set of data</param>
+    /// <param name="decileNumber">The decile for which the average has to be computed</param>
+    static public double GetDecileAverage(double[] data,
+                                         	int decileNumber)
+    {
+    	if(decileNumber<1 || decileNumber >9)
+    		throw new Exception("decileNumber has to be a number between 1 and 9!");
+    	
+    	double firstCorrespondingPercentile =
+    		Percentile(data, decileNumber * 10 - 10);
+    	double secondCorrespondingPercentile = 
+    		Percentile(data, decileNumber * 10);
+    	double totalBetweenPercentiles = 0.0;
+    	int numberOfDataPointsBetweenPercentiles = 0;
+    	for(int i = 0; i<orderedData.Length; i++)
+    	{
+    		if(orderedData[i]>= firstCorrespondingPercentile &&
+    		   orderedData[i]< secondCorrespondingPercentile)
+    		{
+    			numberOfDataPointsBetweenPercentiles++;
+    			totalBetweenPercentiles += orderedData[i];
+    		}
+    		
+    	}
+    	
+    	return totalBetweenPercentiles/numberOfDataPointsBetweenPercentiles;
+    }
+    
+    
 	}	
 }		
 
