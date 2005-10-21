e7bd0295584ad6c4e9222e400ae0f5780570b645
millo_1 <>
2005-10-21 18:07:45 +0000

    Added: - SimpleSelection script; - Open To Close combined with Close to Open; - Open To Close multiday

---
+++ QuantProject/b7_Scripts/TickerSelectionTesting/EndOfDayTimerHandlerOTCCTO.cs
@@ -0,0 +1,224 @@
+/*
+QuantProject - Quantitative Finance Library
+
+EndOfDayTimerHandlerOTCCTO.cs
+Copyright (C) 2003 
+Marco Milletti
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+using System;
+using System.Data;
+using System.Collections;
+
+using QuantProject.ADT;
+using QuantProject.Business.Financial.Accounting;
+using QuantProject.Business.Financial.Instruments;
+using QuantProject.Business.Financial.Ordering;
+using QuantProject.Business.Timing;
+using QuantProject.Data.DataProviders;
+using QuantProject.Data.Selectors;
+using QuantProject.ADT.Optimizing.Genetic;
+
+namespace QuantProject.Scripts.TickerSelectionTesting.EfficientPortfolios
+{
+	
+  /// <summary>
+  /// Implements MarketOpenEventHandler,
+  /// TwoMinutesBeforeMarketCloseEventHandler and OneHourAfterMarketCloseEventHandler
+  /// These handlers contain the core strategy for the efficient close to open portfolio!
+  /// </summary>
+  [Serializable]
+  public class EndOfDayTimerHandlerOTCCTO : EndOfDayTimerHandler
+  {
+    protected int numDaysBetweenEachOptimization;
+    private int numDaysElapsedSinceLastOptimization;
+    protected int seedForRandomGenerator;
+    
+    public EndOfDayTimerHandlerOTCCTO(string tickerGroupID, int numberOfEligibleTickers, 
+                                int numberOfTickersToBeChosen, int numDaysForOptimizationPeriod, Account account,
+                                int generationNumberForGeneticOptimizer,
+                                int populationSizeForGeneticOptimizer,
+                                string benchmark, double targetReturn,
+                                PortfolioType portfolioType, int numDaysBetweenEachOptimization):
+  															base(tickerGroupID, numberOfEligibleTickers, 
+                                numberOfTickersToBeChosen, numDaysForOptimizationPeriod, account,
+                                generationNumberForGeneticOptimizer,
+                                populationSizeForGeneticOptimizer,
+                                benchmark, targetReturn,
+                                portfolioType)
+    {
+    	this.numDaysBetweenEachOptimization = numDaysBetweenEachOptimization;  
+    	this.numDaysElapsedSinceLastOptimization = 0;
+    	this.seedForRandomGenerator = ConstantsProvider.SeedForRandomGenerator;
+    }
+		    
+       
+    /// <summary>
+    /// Handles a "Market Open" event.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="eventArgs"></param>
+    public override void MarketOpenEventHandler(
+      Object sender , EndOfDayTimingEventArgs endOfDayTimingEventArgs )
+    {
+    	//temporarily the if condition
+    	//if(this.numDaysElapsedSinceLastOptimization == 0)
+    	this.closePositions();	
+      this.openPositions();
+    }
+    private void reverseSignOfChosenTickers()
+    {
+      for(int i = 0; i<this.chosenTickers.Length; i++)
+      {
+        if(this.chosenTickers[i] != null)
+        {
+          if(this.chosenTickers[i].StartsWith("-"))
+            this.chosenTickers[i] =
+              GenomeManagerForEfficientPortfolio.GetCleanTickerCode(this.chosenTickers[i]);
+          else
+            this.chosenTickers[i] = "-" + this.chosenTickers[i];
+        }
+      }
+    }
+                
+    public override void MarketCloseEventHandler(
+      Object sender , EndOfDayTimingEventArgs endOfDayTimingEventArgs )
+    {
+    	
+    	//temporarily
+    	//if(this.numDaysElapsedSinceLastOptimization ==
+    	//   this.numDaysBetweenEachOptimization)
+    	this.closePositions();
+      this.orders.Clear();
+      this.reverseSignOfChosenTickers();
+      this.openPositions();
+
+    }
+    
+    
+
+		#region OneHourAfterMarketCloseEventHandler
+      
+    protected DataTable getSetOfTickersToBeOptimized(DateTime currentDate)
+    {
+      /*
+      SelectorByAverageRawOpenPrice selectorByOpenPrice = 
+                  new SelectorByAverageRawOpenPrice(this.tickerGroupID, false,
+                          currentDate.AddDays(-this.numDaysForLiquidity), currentDate,
+                          this.numberOfEligibleTickers, this.minPriceForMinimumCommission,
+                          this.maxPriceForMinimumCommission, 0, 2);
+      DataTable tickersByPrice = selectorByOpenPrice.GetTableOfSelectedTickers();
+      */
+     	
+     	SelectorByGroup temporizedGroup = new SelectorByGroup(this.tickerGroupID, currentDate);
+      SelectorByOpenCloseCorrelationToBenchmark lessCorrelatedFromTemporizedGroup = 
+      	new SelectorByOpenCloseCorrelationToBenchmark(temporizedGroup.GetTableOfSelectedTickers(),
+      	                                              this.benchmark,true,
+      	                                              currentDate.AddDays(-this.numDaysForOptimizationPeriod ),
+      	                                    					currentDate,
+      	                                    					this.numberOfEligibleTickers);
+      
+      this.eligibleTickers = lessCorrelatedFromTemporizedGroup.GetTableOfSelectedTickers();
+      SelectorByQuotationAtEachMarketDay quotedAtEachMarketDayFromEligible = 
+        new SelectorByQuotationAtEachMarketDay( this.eligibleTickers,
+                                   false, currentDate.AddDays(-this.numDaysForOptimizationPeriod),
+                                    currentDate, this.numberOfEligibleTickers, this.benchmark);
+      //SelectorByWinningOpenToClose winners =
+      //	new SelectorByWinningOpenToClose(quotedAtEachMarketDayFromMostLiquid.GetTableOfSelectedTickers(),
+      //	                                 false, currentDate.AddDays(-2),
+      //	                                 currentDate, this.numberOfEligibleTickers/4);      	                                 
+      //return winners.GetTableOfSelectedTickers();
+      //SelectorByOpenCloseCorrelationToBenchmark lessCorrelated = 
+      //  new SelectorByOpenCloseCorrelationToBenchmark(quotedAtEachMarketDayFromEligible.GetTableOfSelectedTickers(),
+      //                                                this.benchmark, true,
+      //                                                currentDate.AddDays(-this.numDaysForLiquidity),
+      //                                                currentDate, this.numberOfEligibleTickers/2);
+      return quotedAtEachMarketDayFromEligible.GetTableOfSelectedTickers();
+      //return lessCorrelated.GetTableOfSelectedTickers();
+    }
+    
+    protected virtual void setTickers(DateTime currentDate,
+                                     	bool setGenomeCounter)
+    {
+      
+      DataTable setOfTickersToBeOptimized = this.getSetOfTickersToBeOptimized(currentDate);
+      if(setOfTickersToBeOptimized.Rows.Count > this.chosenTickers.Length*2)
+        //the optimization process is possible only if the initial set of tickers is 
+        //as large as the number of tickers to be chosen                     
+      
+      {
+        IGenomeManager genManEfficientCTOPortfolio = 
+          new GenomeManagerForEfficientCTOPortfolio(setOfTickersToBeOptimized,
+        	                                          currentDate.AddDays(-this.numDaysForOptimizationPeriod),
+        	                                          currentDate,
+        	                                          this.numberOfTickersToBeChosen,
+        	                                          this.targetReturn,
+        	                                         	this.portfolioType);
+        
+        GeneticOptimizer GO = new GeneticOptimizer(genManEfficientCTOPortfolio,
+                                                    this.populationSizeForGeneticOptimizer,
+                                                    this.generationNumberForGeneticOptimizer,
+                                                   this.seedForRandomGenerator);
+        if(setGenomeCounter)
+        	this.genomeCounter = new GenomeCounter(GO);
+        
+        GO.Run(false);
+        this.addGenomeToBestGenomes(GO.BestGenome,currentDate.AddDays(-this.numDaysForOptimizationPeriod),
+                                    currentDate);
+        this.chosenTickers = (string[])GO.BestGenome.Meaning;
+      }
+      //else it will be buyed again the previous optimized portfolio
+      //that's it the actual chosenTickers member
+    }
+
+    protected void oneHourAfterMarketCloseEventHandler_updatePrices()
+    {
+    	//min price for minimizing commission amount
+    	//according to IB Broker's commission scheme
+    	this.minPriceForMinimumCommission = this.account.CashAmount/(this.numberOfTickersToBeChosen*100);
+      this.maxPriceForMinimumCommission = this.maxPriceForMinimumCommission;
+      //just to avoid warning message
+    }
+    
+    /// <summary>
+    /// Handles a "One hour after market close" event.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="eventArgs"></param>
+    public override void OneHourAfterMarketCloseEventHandler(
+      Object sender , EndOfDayTimingEventArgs endOfDayTimingEventArgs )
+    {
+    	this.seedForRandomGenerator++;
+    	this.orders.Clear();
+    	//this.oneHourAfterMarketCloseEventHandler_updatePrices();
+      if(this.numDaysElapsedSinceLastOptimization == 
+    	   this.numDaysBetweenEachOptimization - 1)
+    	{
+    		this.setTickers(endOfDayTimingEventArgs.EndOfDayDateTime.DateTime, false);
+      	//sets tickers to be chosen next Market Open event
+      	this.numDaysElapsedSinceLastOptimization = 0;
+    	}
+      else
+      {
+        this.numDaysElapsedSinceLastOptimization++;
+      }
+    	
+    }
+		   
+    #endregion
+		
+  }
+}
---
+++ QuantProject/b7_Scripts/TickerSelectionTesting/EndOfDayTimerHandlerOTCMultiday.cs
@@ -0,0 +1,229 @@
+/*
+QuantProject - Quantitative Finance Library
+
+EndOfDayTimerHandlerOTCMultiday.cs
+Copyright (C) 2003 
+Marco Milletti
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+using System;
+using System.Data;
+using System.Collections;
+
+using QuantProject.ADT;
+using QuantProject.Business.Financial.Accounting;
+using QuantProject.Business.Financial.Instruments;
+using QuantProject.Business.Financial.Ordering;
+using QuantProject.Business.Timing;
+using QuantProject.Data.DataProviders;
+using QuantProject.Data.Selectors;
+using QuantProject.ADT.Optimizing.Genetic;
+
+namespace QuantProject.Scripts.TickerSelectionTesting.EfficientPortfolios
+{
+	
+  /// <summary>
+  /// Implements MarketOpenEventHandler,
+  /// TwoMinutesBeforeMarketCloseEventHandler and OneHourAfterMarketCloseEventHandler
+  /// These handlers contain the core strategy for the efficient close to open portfolio!
+  /// </summary>
+  [Serializable]
+  public class EndOfDayTimerHandlerOTCMultiday : EndOfDayTimerHandler
+  {
+    protected int numDaysBetweenEachOptimization;
+    private int numDaysElapsedSinceLastOptimization;
+    protected int seedForRandomGenerator;
+    
+    public EndOfDayTimerHandlerOTCMultiday(string tickerGroupID, int numberOfEligibleTickers, 
+                                int numberOfTickersToBeChosen, int numDaysForOptimizationPeriod, Account account,
+                                int generationNumberForGeneticOptimizer,
+                                int populationSizeForGeneticOptimizer,
+                                string benchmark, double targetReturn,
+                                PortfolioType portfolioType, int numDaysBetweenEachOptimization):
+  															base(tickerGroupID, numberOfEligibleTickers, 
+                                numberOfTickersToBeChosen, numDaysForOptimizationPeriod, account,
+                                generationNumberForGeneticOptimizer,
+                                populationSizeForGeneticOptimizer,
+                                benchmark, targetReturn,
+                                portfolioType)
+    {
+    	this.numDaysBetweenEachOptimization = numDaysBetweenEachOptimization;  
+    	this.numDaysElapsedSinceLastOptimization = 0;
+    	this.seedForRandomGenerator = ConstantsProvider.SeedForRandomGenerator;
+    }
+		    
+       
+    /// <summary>
+    /// Handles a "Market Open" event.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="eventArgs"></param>
+    public override void MarketOpenEventHandler(
+      Object sender , EndOfDayTimingEventArgs endOfDayTimingEventArgs )
+    {
+    	
+    	if(this.numDaysElapsedSinceLastOptimization == 0)
+    			this.openPositions();
+    }
+		
+                
+    public override void MarketCloseEventHandler(
+      Object sender , EndOfDayTimingEventArgs endOfDayTimingEventArgs )
+    {
+    	
+    	
+    	if(this.numDaysElapsedSinceLastOptimization ==
+    	   this.numDaysBetweenEachOptimization - 1)
+    	 			this.closePositions();
+    	  	
+    }
+    
+    
+
+		#region OneHourAfterMarketCloseEventHandler
+      
+    protected DataTable getSetOfTickersToBeOptimized(DateTime currentDate)
+    {
+      /*
+      SelectorByAverageRawOpenPrice selectorByOpenPrice = 
+                  new SelectorByAverageRawOpenPrice(this.tickerGroupID, false,
+                          currentDate.AddDays(-this.numDaysForLiquidity), currentDate,
+                          this.numberOfEligibleTickers, this.minPriceForMinimumCommission,
+                          this.maxPriceForMinimumCommission, 0, 2);
+      DataTable tickersByPrice = selectorByOpenPrice.GetTableOfSelectedTickers();
+      */
+     	
+     	SelectorByGroup temporizedGroup = new SelectorByGroup(this.tickerGroupID, currentDate);
+      SelectorByOpenCloseCorrelationToBenchmark lessCorrelatedFromTemporizedGroup = 
+      	new SelectorByOpenCloseCorrelationToBenchmark(temporizedGroup.GetTableOfSelectedTickers(),
+      	                                              this.benchmark,true,
+      	                                              currentDate.AddDays(-this.numDaysForOptimizationPeriod ),
+      	                                    					currentDate,
+      	                                    					this.numberOfEligibleTickers);
+      
+      this.eligibleTickers = lessCorrelatedFromTemporizedGroup.GetTableOfSelectedTickers();
+      SelectorByQuotationAtEachMarketDay quotedAtEachMarketDayFromEligible = 
+        new SelectorByQuotationAtEachMarketDay( this.eligibleTickers,
+                                   false, currentDate.AddDays(-this.numDaysForOptimizationPeriod),
+                                    currentDate, this.numberOfEligibleTickers, this.benchmark);
+      //SelectorByWinningOpenToClose winners =
+      //	new SelectorByWinningOpenToClose(quotedAtEachMarketDayFromMostLiquid.GetTableOfSelectedTickers(),
+      //	                                 false, currentDate.AddDays(-2),
+      //	                                 currentDate, this.numberOfEligibleTickers/4);      	                                 
+      //return winners.GetTableOfSelectedTickers();
+      //SelectorByOpenCloseCorrelationToBenchmark lessCorrelated = 
+      //  new SelectorByOpenCloseCorrelationToBenchmark(quotedAtEachMarketDayFromEligible.GetTableOfSelectedTickers(),
+      //                                                this.benchmark, true,
+      //                                                currentDate.AddDays(-this.numDaysForLiquidity),
+      //                                                currentDate, this.numberOfEligibleTickers/2);
+      return quotedAtEachMarketDayFromEligible.GetTableOfSelectedTickers();
+      //return lessCorrelated.GetTableOfSelectedTickers();
+    }
+    
+    private string[] setTickers_getBestTickersLosingAtLastDay(IGenomeManager genomeManager)
+    {
+    	string[] returnValue =
+    		((MeaningForGenome)genomeManager.CurrentGeneticOptimizer.BestGenome.Meaning).Tickers;
+    	int numOfGenomes = genomeManager.CurrentGeneticOptimizer.CurrentGeneration.Count;
+    	int i;
+    	for(i = 0;i<numOfGenomes; i++)
+    	{
+    		if(((MeaningForGenome)((Genome)genomeManager.CurrentGeneticOptimizer.CurrentGeneration[numOfGenomes - i - 1]).Meaning).ReturnAtLastDayInSample < 0)
+    		//the current portfolio has a negative return at the last day in sample
+    		{
+    			returnValue =
+    					((MeaningForGenome)((Genome)genomeManager.CurrentGeneticOptimizer.CurrentGeneration[numOfGenomes - i - 1]).Meaning).Tickers;
+    			i = numOfGenomes;
+    		}
+    	}
+    
+    	return returnValue;
+    }
+    
+    protected virtual void setTickers(DateTime currentDate,
+                                     	bool setGenomeCounter)
+    {
+      
+      DataTable setOfTickersToBeOptimized = this.getSetOfTickersToBeOptimized(currentDate);
+      if(setOfTickersToBeOptimized.Rows.Count > this.chosenTickers.Length*2)
+        //the optimization process is possible only if the initial set of tickers is 
+        //as large as the number of tickers to be chosen                     
+      
+      {
+        IGenomeManager genManEfficientCTOPortfolio = 
+          new GenomeManagerForEfficientCTOPortfolio(setOfTickersToBeOptimized,
+        	                                          currentDate.AddDays(-this.numDaysForOptimizationPeriod),
+        	                                          currentDate,
+        	                                          this.numberOfTickersToBeChosen,
+        	                                          this.targetReturn,
+        	                                         	this.portfolioType);
+        
+        GeneticOptimizer GO = new GeneticOptimizer(genManEfficientCTOPortfolio,
+                                                    this.populationSizeForGeneticOptimizer,
+                                                    this.generationNumberForGeneticOptimizer,
+                                                   this.seedForRandomGenerator);
+        if(setGenomeCounter)
+        	this.genomeCounter = new GenomeCounter(GO);
+        
+        GO.Run(false);
+        this.addGenomeToBestGenomes(GO.BestGenome,currentDate.AddDays(-this.numDaysForOptimizationPeriod),
+                                    currentDate);
+        //this.chosenTickers = ((MeaningForGenome)GO.BestGenome.Meaning).Tickers;
+        this.chosenTickers =
+        	this.setTickers_getBestTickersLosingAtLastDay(genManEfficientCTOPortfolio);
+      }
+      //else it will be buyed again the previous optimized portfolio
+      //that's it the actual chosenTickers member
+    }
+
+    protected void oneHourAfterMarketCloseEventHandler_updatePrices()
+    {
+    	//min price for minimizing commission amount
+    	//according to IB Broker's commission scheme
+    	this.minPriceForMinimumCommission = this.account.CashAmount/(this.numberOfTickersToBeChosen*100);
+      this.maxPriceForMinimumCommission = this.maxPriceForMinimumCommission;
+      //just to avoid warning message
+    }
+    
+    /// <summary>
+    /// Handles a "One hour after market close" event.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="eventArgs"></param>
+    public override void OneHourAfterMarketCloseEventHandler(
+      Object sender , EndOfDayTimingEventArgs endOfDayTimingEventArgs )
+    {
+    	this.seedForRandomGenerator++;
+    	this.orders.Clear();
+    	//this.oneHourAfterMarketCloseEventHandler_updatePrices();
+      if(this.numDaysElapsedSinceLastOptimization == 
+    	   this.numDaysBetweenEachOptimization - 1)
+    	{
+    		this.setTickers(endOfDayTimingEventArgs.EndOfDayDateTime.DateTime, false);
+      	//sets tickers to be chosen next Market Open event
+      	this.numDaysElapsedSinceLastOptimization = 0;
+    	}
+      else
+      {
+        this.numDaysElapsedSinceLastOptimization++;
+      }
+    	
+    }
+		   
+    #endregion
+		
+  }
+}
