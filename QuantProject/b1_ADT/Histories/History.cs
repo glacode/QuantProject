a97cae7e59f46c9de4055269e6bbefbc9513ddd4
glauco_1 <>
2006-06-18 14:22:14 +0000

    A couple of ambiguous Math.Floor arguments have been converted to double (framework 1.x accepted such ambiguity, but framework 2.x doesn't)

--- QuantProject/b1_ADT/Histories/History.cs
+++ QuantProject/b1_ADT/Histories/History.cs
@@ -203,8 +203,9 @@ namespace QuantProject.ADT.Histories
 				( ((IComparable)this.GetKey( currentHistoryIndex )).CompareTo( endDateTime ) <= 0 ) )
 			{
 			  
-				if (Math.Floor(currentHistoryIndex/onEachPeriodOf) == periodIndex &&
-					cursorThroughDataArray < onEachPeriodOf)
+				if ( Math.Floor( Convert.ToDouble(
+					currentHistoryIndex / onEachPeriodOf ) ) == periodIndex &&
+					cursorThroughDataArray < onEachPeriodOf )
 					//currentHistoryIndex belongs to the current period
 				{	
 					data[cursorThroughDataArray] = Convert.ToDouble(this.GetByIndex(currentHistoryIndex));
@@ -233,7 +234,8 @@ namespace QuantProject.ADT.Histories
 							break;
 					}
 				}
-				periodIndex = Math.Floor(currentHistoryIndex/onEachPeriodOf);
+				periodIndex = Math.Floor( Convert.ToDouble(
+					currentHistoryIndex/onEachPeriodOf ) );
 			}
 
 			return functionHistory;
