4f781045246aa9c7d00539c48992f9548484cdff
glauco_1 <>
2006-06-24 14:46:00 +0000

    The ISerializable interface is implemented now. I suggest to read the interface implementation, to replicate it for other serializable classes expected to enrich in future releases.

--- QuantProject/b7_Scripts/WalkForwardTesting/WalkForwardLag/WFLagDebugger/WFLagLog.cs
+++ QuantProject/b7_Scripts/WalkForwardTesting/WalkForwardLag/WFLagDebugger/WFLagLog.cs
@@ -21,9 +21,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 using System;
+using System.Reflection;
+using System.Runtime.Serialization;
 
 using QuantProject.ADT.Histories;
-
 using QuantProject.Business.Financial.Accounting.Transactions;
 
 namespace QuantProject.Scripts.WalkForwardTesting.WalkForwardLag.WFLagDebugger
@@ -32,7 +33,7 @@ namespace QuantProject.Scripts.WalkForwardTesting.WalkForwardLag.WFLagDebugger
 	/// Log for a walk forward lag backtest
 	/// </summary>
 	[Serializable]
-	public class WFLagLog
+	public class WFLagLog : ISerializable
 	{
 		private int inSampleDays;
 		private string benchmark;
@@ -55,12 +56,62 @@ namespace QuantProject.Scripts.WalkForwardTesting.WalkForwardLag.WFLagDebugger
 			set { this.transactionHistory = value; }
 		}
 
+		public History ChosenPositionsHistory
+		{
+			get { return this.chosenPositionsHistory; }
+		}
 		public WFLagLog( int inSampleDays , string benchmark )
 		{
 			this.inSampleDays = inSampleDays;
 			this.benchmark = benchmark;
 			this.chosenPositionsHistory = new History();
 		}
+		/// <summary>
+		/// This constructor allows custom deserialization (see the ISerializable
+		/// interface documentation)
+		/// </summary>
+		/// <param name="info"></param>
+		/// <param name="context"></param>
+		protected WFLagLog( SerializationInfo info , StreamingContext context )
+		{
+			// get the set of serializable members for this class and its base classes
+			Type thisType = this.GetType();
+			MemberInfo[] mi = FormatterServices.GetSerializableMembers(
+				thisType , context);
+
+			// deserialize the fields from the info object
+			for (Int32 i = 0 ; i < mi.Length; i++) 
+			{
+				FieldInfo fieldInfo = (FieldInfo) mi[i];
+
+				// set the field to the deserialized value
+				fieldInfo.SetValue( this ,
+					info.GetValue( fieldInfo.Name, fieldInfo.FieldType ) );
+			}
+
+		}
+		#region GetObjectData
+		/// <summary>
+		/// serialize the set of serializable members for this class and base classes
+		/// </summary>
+		/// <param name="info"></param>
+		/// <param name="context"></param>
+		void ISerializable.GetObjectData(
+			SerializationInfo info, StreamingContext context) 
+		{
+			// get the set of serializable members for this class and base classes
+			Type thisType = this.GetType();
+			MemberInfo[] mi = 
+				FormatterServices.GetSerializableMembers( thisType , context);
+
+			// serialize the fields to the info object
+			for (Int32 i = 0 ; i < mi.Length; i++) 
+			{
+				info.AddValue(mi[i].Name, ((FieldInfo) mi[i]).GetValue(this));
+			}
+		}
+		#endregion
+
 		public void Add( WFLagChosenPositions wFLagChosenPositions )
 		{
 			this.chosenPositionsHistory.Add(
