ded4a13a4c247387b5327fdff5e8b94b560fcd81
millo_1 <>
2006-07-02 19:42:15 +0000

    Updated RunEfficientOTCTypes

--- QuantProject/b7_Scripts/TickerSelectionTesting/TestingOTCTypes/RunEfficientOTCTypes.cs
+++ QuantProject/b7_Scripts/TickerSelectionTesting/TestingOTCTypes/RunEfficientOTCTypes.cs
@@ -71,7 +71,7 @@ namespace QuantProject.Scripts.TickerSelectionTesting.EfficientPortfolios
       startDate, endDate, targetReturn,
       portfolioType, maxRunningHours)
     {
-      this.ScriptName = "OTC_Types_SharpeRatioCombinedNoCoeff";
+      this.ScriptName = "OTC_Types_ExpScoreWithCoeff";
       this.numDaysBetweenEachOptimization = numDaysBetweenEachOptimization;
       this.accounts = new Account[4];
     }
