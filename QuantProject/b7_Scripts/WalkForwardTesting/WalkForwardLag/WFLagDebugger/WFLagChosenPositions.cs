9a1a7f018f9e0b90fb2a2199b12a5bc3cc80f62e
glauco_1 <>
2006-07-30 13:50:26 +0000

    - WeightedPositions are used instead of SignedTickers - now the ISerializable interface is implemented, so that old logs can be examined yet - it may be worth looking at the deserializing code (not that simple...), as an example for future custom deserialization (if and when needed)

--- QuantProject/b7_Scripts/WalkForwardTesting/WalkForwardLag/WFLagDebugger/WFLagChosenPositions.cs
+++ QuantProject/b7_Scripts/WalkForwardTesting/WalkForwardLag/WFLagDebugger/WFLagChosenPositions.cs
@@ -21,8 +21,12 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 using System;
+using System.Collections;
+using System.Reflection;
+using System.Runtime.Serialization;
 
 using QuantProject.ADT.Collections;
+using QuantProject.Business.Strategies;
 using QuantProject.Scripts.WalkForwardTesting.WalkForwardLag;
 
 namespace QuantProject.Scripts.WalkForwardTesting.WalkForwardLag.WFLagDebugger
@@ -31,24 +35,34 @@ namespace QuantProject.Scripts.WalkForwardTesting.WalkForwardLag.WFLagDebugger
 	/// Creates a copy of the relevant data for the WFLagChosenTickers object
 	/// </summary>
 	[Serializable]
-	public class WFLagChosenPositions
+	public class WFLagChosenPositions : ISerializable
 	{
+		// these private members are used for old versions' deserialization, only
 		private QPHashtable drivingPositions;
 		private QPHashtable portfolioPositions;
+
+		private WeightedPositions drivingWeightedPositions;
+		private WeightedPositions portfolioWeightedPositions;
 		private DateTime lastOptimizationDate;
 
-		public QPHashtable DrivingPositions
+		public WeightedPositions DrivingWeightedPositions
 		{
 			get
 			{
-				return this.drivingPositions;
+				if ( this.drivingWeightedPositions == null )
+					// an old version has been deserialized
+					this.setWeightedPositionsFromQPHashtables();
+				return this.drivingWeightedPositions;
 			}
 		}
-		public QPHashtable PortfolioPositions
+		public WeightedPositions PortfolioWeightedPositions
 		{
 			get
 			{
-				return this.portfolioPositions;
+				if ( this.portfolioWeightedPositions == null )
+					// an old version has been deserialized
+					this.setWeightedPositionsFromQPHashtables();
+				return this.portfolioWeightedPositions;
 			}
 		}
 		public DateTime LastOptimizationDate
@@ -62,19 +76,157 @@ namespace QuantProject.Scripts.WalkForwardTesting.WalkForwardLag.WFLagDebugger
 		public WFLagChosenPositions( WFLagChosenTickers wFLagChosenTickers ,
 			DateTime lastOptimizationDate )
 		{
-			this.drivingPositions =
-				this.copy( wFLagChosenTickers.DrivingPositions );
-			this.portfolioPositions =
-				this.copy( wFLagChosenTickers.PortfolioPositions );
+			//			this.drivingPositions =
+			//				this.copy( wFLagChosenTickers.DrivingWeightedPositions );
+			//			this.portfolioPositions =
+			//				this.copy( wFLagChosenTickers.PortfolioWeightedPositions );
+			this.drivingWeightedPositions = wFLagChosenTickers.DrivingWeightedPositions;
+			this.portfolioWeightedPositions = wFLagChosenTickers.PortfolioWeightedPositions;
 			this.lastOptimizationDate = lastOptimizationDate;
 		}
 
-		private QPHashtable copy( QPHashtable qPHashTable )
+		#region deserialization related constructor and methods
+		protected WFLagChosenPositions( SerializationInfo info , StreamingContext context )
 		{
-			QPHashtable newCopy = new QPHashtable();
-			foreach ( string key in qPHashTable.Keys )
-				newCopy.Add( key , null );
-			return newCopy;
+			this.deserializeBaseClassMembers( info , context );
+			this.deserializeThisClassMembers( info , context );
 		}
+		private void deserializeBaseClassMembers( SerializationInfo info , StreamingContext context )
+		{
+			// get the set of serializable members for this class and its base classes
+			Type thisType = this.GetType();
+			MemberInfo[] memberInfos = FormatterServices.GetSerializableMembers(
+				thisType , context);
+
+			// deserialize the fields from the info object, only if of the base clas
+			for (Int32 i = 0 ; i < memberInfos.Length; i++) 
+			{
+				// Don't deserialize fields for this class
+				if (memberInfos[i].DeclaringType != thisType)
+				{
+					FieldInfo fieldInfo = (FieldInfo) memberInfos[i];
+
+					// set the field to the deserialized value
+					fieldInfo.SetValue( this ,
+						info.GetValue( fieldInfo.Name, fieldInfo.FieldType ) );
+				}
+			}
+		}
+		private void deserializeDrivingWeightedPositions(
+			SerializationInfo info , StreamingContext context )
+		{
+			try
+			{
+//				this.drivingWeightedPositions = new WeightedPositions()
+//				System.Type type = System.Type.GetType(
+//					"QuantProject.Scripts.WalkForwardTesting.WalkForwardLag.WeightedPositions" );
+				this.drivingWeightedPositions = (WeightedPositions)info.GetValue(
+					"drivingWeightedPositions" , WeightedPositions.Type );
+			}
+			catch( Exception ex1 )
+			{
+				// the serialized WFLagChosenPositions is of old type
+				// drivingPositions and portfolioPositions are QPHashtable
+				try
+				{
+					string errorMessage1 = ex1.Message;
+					this.drivingPositions = new QPHashtable();
+					this.drivingPositions = (QPHashtable)info.GetValue(
+						"drivingPositions" , this.drivingPositions.GetType() );
+					//				drivingWeightedPositions = this.getWeightedPositions( drivingPositions );
+				}
+				catch( Exception ex2 )
+				{
+					string errorMessage = ex2.Message;
+					errorMessage = errorMessage;
+				}
+			}
+		}
+		private void deserializePortfolioWeightedPositions(
+			SerializationInfo info , StreamingContext context )
+		{
+			try
+			{
+//				this.portfolioWeightedPositions = new WeightedPositions();
+//				System.Type type = System.Type.GetType(
+//					"QuantProject.Scripts.WalkForwardTesting.WalkForwardLag.WeightedPositions" );
+				this.portfolioWeightedPositions = (WeightedPositions)info.GetValue(
+					"portfolioWeightedPositions" , WeightedPositions.Type );
+			}
+			catch
+			{
+				// the serialized WFLagChosenPositions is of old type
+				// drivingPositions and portfolioPositions are QPHashtable
+				this.portfolioPositions = new QPHashtable();
+				this.portfolioPositions = (QPHashtable)info.GetValue(
+					"portfolioPositions" , this.portfolioPositions.GetType() );
+				//				portfolioWeightedPositions = this.getWeightedPositions( portfolioPositions );
+			}
+		}
+		private void deserializeThisClassMembers( SerializationInfo info , StreamingContext context )
+		{
+			this.lastOptimizationDate = (DateTime)info.GetValue( "lastOptimizationDate" ,
+				this.lastOptimizationDate.GetType() );
+			this.deserializeDrivingWeightedPositions( info , context );
+			this.deserializePortfolioWeightedPositions( info , context );
+		}
+		void ISerializable.GetObjectData(
+			SerializationInfo info, StreamingContext context) 
+		{
+			// get the set of serializable members for this class and base classes
+			Type thisType = this.GetType();
+			MemberInfo[] mi = 
+				FormatterServices.GetSerializableMembers( thisType , context);
+
+			// serialize the fields to the info object
+			for (Int32 i = 0 ; i < mi.Length; i++) 
+			{ 
+				string memberName = mi[i].Name;
+				if ( ( memberName != "drivingPositions" ) && ( memberName != "portfolioPositions" ) )
+					// current member is not used for old versions' deserialization, only
+					info.AddValue(mi[i].Name, ((FieldInfo) mi[i]).GetValue(this));
+			}
+		}
+//		private Hashtable copy( Hashtable hashTable )
+//		{
+//			Hashtable newCopy = new Hashtable();
+//			foreach ( string key in hashTable.Keys )
+//				newCopy.Add( key , null );
+//			return newCopy;
+//		}
+		#region setWeightedPositionsFromQPHashtables
+		private double getWeightedPositions_getWeight(
+			double absoluteWeightForEachPosition , string signedTicker )
+		{
+			double weight = absoluteWeightForEachPosition;
+			if ( SignedTicker.IsShort( signedTicker ) )
+				weight = - absoluteWeightForEachPosition;
+			return weight;
+		}
+		private WeightedPositions getWeightedPositions( QPHashtable signedTickers )
+		{
+			double absoluteWeightForEachPosition =
+				1 / Convert.ToDouble( signedTickers.Count );
+			double[] weights = new double[ signedTickers.Count ];
+			string[] tickers = new string[ signedTickers.Count ];
+			int i = 0;
+			foreach ( string signedTicker in signedTickers.Keys )
+			{
+				weights[ i ] = this.getWeightedPositions_getWeight(
+					absoluteWeightForEachPosition , signedTicker );
+				tickers[ i ] = SignedTicker.GetTicker( signedTicker );
+				i++;
+			}
+			WeightedPositions weightedPositions =
+				new WeightedPositions( weights , tickers );
+			return weightedPositions;
+		}
+		private void setWeightedPositionsFromQPHashtables()
+		{
+			this.drivingWeightedPositions = this.getWeightedPositions( this.drivingPositions );
+			this.portfolioWeightedPositions = this.getWeightedPositions( this.portfolioPositions );
+		}
+		#endregion
+		#endregion
 	}
 }
