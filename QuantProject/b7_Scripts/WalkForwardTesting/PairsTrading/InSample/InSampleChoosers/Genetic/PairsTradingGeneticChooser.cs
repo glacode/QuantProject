dc1e859698bdf365d6f8524e7c47b0ed2efbd7b2
glauco_1 <>
2008-02-27 21:16:20 +0000

    genetic IInSampleChooser for the pairs trading strategy

---
+++ QuantProject/b7_Scripts/WalkForwardTesting/PairsTrading/InSample/InSampleChoosers/Genetic/PairsTradingGeneticChooser.cs
@@ -0,0 +1,91 @@
+/*
+QuantProject - Quantitative Finance Library
+
+PairsTradingGeneticChooser.cs
+Copyright (C) 2008
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+using System;
+
+using QuantProject.ADT.Optimizing.Genetic;
+using QuantProject.Business.DataProviders;
+using QuantProject.Business.Strategies;
+using QuantProject.Business.Strategies.Eligibles;
+using QuantProject.Business.Strategies.InSample;
+using QuantProject.Business.Strategies.Optimizing.Decoding;
+using QuantProject.Business.Strategies.Optimizing.FitnessEvaluation;
+using QuantProject.Business.Strategies.Optimizing.GenomeManagers;
+using QuantProject.Business.Strategies.OutOfSample;
+using QuantProject.Business.Strategies.ReturnsManagement;
+
+namespace QuantProject.Scripts.WalkForwardTesting.PairsTrading
+{
+	/// <summary>
+	/// genetic IInSampleChooser for the pairs trading strategy
+	/// </summary>
+	public class PairsTradingGeneticChooser : GeneticChooser
+	{
+		public PairsTradingGeneticChooser(
+			int numberOfBestTestingPositionsToBeReturned ,
+			Benchmark benchmark ,
+			IDecoderForTestingPositions decoderForTestingPositions ,
+			IFitnessEvaluator fitnessEvaluator ,
+			IHistoricalQuoteProvider historicalQuoteProvider ,
+			double crossoverRate , double mutationRate , double elitismRate ,
+			int populationSizeForGeneticOptimizer ,
+			int generationNumberForGeneticOptimizer ,
+			int seedForRandomGenerator ) :
+			base(
+				2 ,
+				numberOfBestTestingPositionsToBeReturned ,
+				benchmark ,
+				decoderForTestingPositions ,
+				fitnessEvaluator ,
+				historicalQuoteProvider ,
+				crossoverRate ,
+				mutationRate ,
+				elitismRate ,
+				populationSizeForGeneticOptimizer ,
+				generationNumberForGeneticOptimizer ,
+				seedForRandomGenerator )
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+		protected override IGenomeManager getGenomeManager(
+			EligibleTickers eligibleTickers ,
+			ReturnsManager returnsManager )
+		{
+			GenomeManagerWithDuplicateGenes	genomeManagerWithDuplicateGenes =
+				new GenomeManagerWithDuplicateGenes(
+				2 ,
+				eligibleTickers ,
+				returnsManager ,
+				this.decoderForTestingPositions ,
+				this.fitnessEvaluator ,
+				GenomeManagerType.ShortAndLong ,
+				this.seedForRandomGenerator );
+			return genomeManagerWithDuplicateGenes;
+		}
+		protected override string getHashCodeForGenome( Genome genome )
+		{
+			return ((TestingPositions)(genome.Meaning)).HashCodeForTickerComposition;
+		}
+	}
+}
