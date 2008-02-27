0b0e3fc89234f486fc9d58ed4296d845a6a8983d
glauco_1 <>
2008-02-27 22:15:19 +0000

    Minor change: using statements have been alphabetically reordered

--- QuantProject/b4_Business/a2_Strategies/Optimizing/Decoding/IDecoderForTestingPositions.cs
+++ QuantProject/b4_Business/a2_Strategies/Optimizing/Decoding/IDecoderForTestingPositions.cs
@@ -23,9 +23,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 using System;
 
 using QuantProject.Business.Strategies.Eligibles;
+using QuantProject.Business.Strategies.Logging;
 using QuantProject.Business.Strategies.OutOfSample;
 using QuantProject.Business.Strategies.ReturnsManagement;
-using QuantProject.Business.Strategies.Logging;
 
 namespace QuantProject.Business.Strategies.Optimizing.Decoding
 {
