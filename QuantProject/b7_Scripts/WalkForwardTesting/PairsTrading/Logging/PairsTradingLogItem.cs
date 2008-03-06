e65988a8e0fba91a34d0c5583a9a6afc8656f305
glauco_1 <>
2008-03-06 20:18:40 +0000

    The Run method has been completely implemented, now

--- QuantProject/b7_Scripts/WalkForwardTesting/PairsTrading/Logging/PairsTradingLogItem.cs
+++ QuantProject/b7_Scripts/WalkForwardTesting/PairsTrading/Logging/PairsTradingLogItem.cs
@@ -21,8 +21,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 using System;
+using System.Drawing;
 
 using QuantProject.Business.DataProviders;
+using QuantProject.Business.Financial.Accounting.Reporting;
 using QuantProject.Business.Strategies;
 using QuantProject.Business.Strategies.Eligibles;
 using QuantProject.Business.Strategies.InSample;
@@ -30,6 +32,7 @@ using QuantProject.Business.Strategies.Logging;
 using QuantProject.Business.Strategies.OutOfSample;
 using QuantProject.Business.Strategies.ReturnsManagement.Time.IntervalsSelectors;
 using QuantProject.Business.Timing;
+using QuantProject.Presentation.Reporting.WindowsForm;
 using QuantProject.Scripts.General.Reporting;
 
 namespace QuantProject.Scripts.WalkForwardTesting.PairsTrading
@@ -74,44 +77,89 @@ namespace QuantProject.Scripts.WalkForwardTesting.PairsTrading
 			this.bestTestingPositionsInSample = bestTestingPositionsInSample;
 			this.numberOfEligibleTickers = numberOfEligibleTickers;
 		}
+		private AccountReport getAccountReport(
+			WeightedPositions weightedPositions ,
+			IIntervalsSelector intervalsSelector ,
+			IHistoricalQuoteProvider historicalQuoteProvider ,
+			Benchmark benchmark ,
+			double cashToStart )
+		{
+			SimpleStrategy simpleStrategy =
+				new SimpleStrategy( weightedPositions ,
+				intervalsSelector , historicalQuoteProvider );
+
+			DateTime firstDateTime = this.simulatedCreationTime.DateTime.AddDays( -90 );
+			DateTime lastDateTime = this.simulatedCreationTime.DateTime;
+			double maxRunningHours = 0.3;
+			EndOfDayStrategyBackTester endOfDayStrategyBackTester =
+				new EndOfDayStrategyBackTester(
+				"SinglePosition" , simpleStrategy ,
+				historicalQuoteProvider , firstDateTime ,
+				lastDateTime , benchmark , cashToStart , maxRunningHours );
+
+			simpleStrategy.Account = endOfDayStrategyBackTester.Account;
+
+			endOfDayStrategyBackTester.Run();
+			return endOfDayStrategyBackTester.AccountReport;
+		}
+		private WeightedPositions getWeightedPositions(
+			WeightedPosition weightedPosition )
+		{
+			double[] weights = { 1 };
+			string[] tickers = { weightedPosition.Ticker };
+			WeightedPositions weightedPositions =
+				new WeightedPositions( weights , tickers );
+			return weightedPositions;
+		}
 		public override void Run()
 		{
-//			string backTestId = "SimpleFLTP";
+//			string backTestId = "SimplePairsTrading";
 //			double cashToStart = 30000;
-//
-//			Benchmark benchmark = new Benchmark( "MSFT" );
-//
-//			IHistoricalQuoteProvider historicalQuoteProvider =
-//				new HistoricalAdjustedQuoteProvider();
-//
+
+			Benchmark benchmark = new Benchmark( "MSFT" );
+
+			IHistoricalQuoteProvider historicalQuoteProvider =
+				new HistoricalAdjustedQuoteProvider();
+
 //			IInSampleChooser inSampleChooser =
-//				(IInSampleChooser)new ConstantWeightedPositionsChooser( this.BestWeightedPositionsInSample );
-//
-//			IIntervalsSelector intervalsSelector =
-//				new FixedLengthTwoPhasesIntervalsSelector(
-//				1 , 1 , benchmark );
-//			IEligiblesSelector eligiblesSelector = new DummyEligibleSelector();
-//
-//			FixedLengthTwoPhasesStrategy fixedLengthTwoPhasesStrategy =
-//				new FixedLengthTwoPhasesStrategy(
-//				this.BestWeightedPositionsInSample.Count ,
-//				9999 , 9 , benchmark , intervalsSelector ,
-//				eligiblesSelector , inSampleChooser , historicalQuoteProvider );
-//
-//			DateTime firstDateTime = this.simulatedCreationTime.DateTime.AddDays( -90 );
-//			DateTime lastDateTime = this.simulatedCreationTime.DateTime;
-//			double maxRunningHours = 0.3;
-//			EndOfDayStrategyBackTester endOfDayStrategyBackTester =
-//				new EndOfDayStrategyBackTester(
-//				backTestId , fixedLengthTwoPhasesStrategy ,
-//				historicalQuoteProvider , firstDateTime ,
-//				lastDateTime , benchmark , cashToStart , maxRunningHours );
-//
-//			fixedLengthTwoPhasesStrategy.Account = endOfDayStrategyBackTester.Account;
-//
-//			endOfDayStrategyBackTester.Run();
-//			BackTesterReportViewer.ShowReport( lastDateTime ,
-//				endOfDayStrategyBackTester );
+//				(IInSampleChooser)new ConstantWeightedPositionsChooser(
+//				this.BestWeightedPositionsInSample );
+
+			IIntervalsSelector intervalsSelector =
+				new OddIntervalsSelector(	1 , 1 , benchmark );
+			IEligiblesSelector eligiblesSelector = new DummyEligibleSelector();
+
+			WeightedPositions weightedPositions =
+				this.bestTestingPositionsInSample[ 0 ].WeightedPositions;
+				
+			WeightedPositions firstPosition =
+				this.getWeightedPositions( weightedPositions[ 0 ] );
+			WeightedPositions secondPosition =
+				this.getWeightedPositions( weightedPositions[ 1 ] );
+			AccountReport accountReportForFirstPosition =
+				this.getAccountReport( firstPosition , intervalsSelector ,
+				                     historicalQuoteProvider ,
+				                     benchmark , 30000 );
+			AccountReport accountReportForSecondPosition =
+				this.getAccountReport( secondPosition , intervalsSelector ,
+				                      historicalQuoteProvider ,
+				                      benchmark ,
+				                      30000 * weightedPositions[ 1 ].Weight /
+				                      weightedPositions[ 0 ].Weight );
+			
+			Report report =
+				new Report( accountReportForFirstPosition , false );
+			EndOfDayDateTime lastEndOfDayDateTimeForReport =
+				new EndOfDayDateTime(
+					accountReportForFirstPosition.EquityLine.LastDateTime ,
+					EndOfDaySpecificTime.OneHourAfterMarketClose );
+				
+//			report.Create( "PearsonDebug" , 1 ,
+//			              lastEndOfDayDateTimeForReport ,
+//			              benchmark.Ticker , false );
+			report.AddEquityLine( accountReportForSecondPosition.EquityLine ,
+			                     Color.Brown );
+			report.ShowDialog();
 		}
 	}
 }
