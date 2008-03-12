7e57dd83a71abba3ffad13a233c1d1c70215bbd4
millo_1 <>
2008-03-12 21:51:35 +0000

    Added OutOfRangeException, the exception that should be thrown when a given number does not fit the expected range

---
+++ QuantProject/b1_ADT/OutOfRangeException.cs
@@ -0,0 +1,55 @@
+/*
+QuantProject - Quantitative Finance Library
+
+OutOfRangeException.cs
+Copyright (C) 2008 
+Marco Milletti
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+using System;
+
+namespace QuantProject.ADT
+{
+	/// <summary>
+	/// It should be thrown when a given number
+	/// is out of the desired range
+	/// </summary>
+	public class OutOfRangeException : Exception
+	{
+		private double outOfRangeNumber;
+		private double minimumForValidRange;
+		private double maximumForValidRange;
+		
+		public override string Message
+		{
+			get
+			{
+				return this.outOfRangeNumber.ToString() + 
+							 "is out of range! It should be between " +
+							 this.minimumForValidRange.ToString() + " and " +
+					     this.maximumForValidRange.ToString();
+			}
+		}
+		public OutOfRangeException( double outOfRangeNumber,
+		                            double minimumForValidRange,
+		                            double maximumForValidRange)
+		{
+			this.outOfRangeNumber = outOfRangeNumber;
+			this.minimumForValidRange = minimumForValidRange;
+			this.maximumForValidRange = maximumForValidRange;
+		}
+	}
+}
