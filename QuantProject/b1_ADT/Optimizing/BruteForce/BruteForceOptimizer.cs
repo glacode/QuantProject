79edd2ee4e42fd68ac13065bdec51e6aa9abdbbf
glauco_1 <>
2008-03-26 00:46:38 +0000

    - the read only property public BruteForceOptimizableParameters[] TopBestParameters has been added
    - a new parameter
    int numberOfTopBestParameters
    has been added to the constructor

--- QuantProject/b1_ADT/Optimizing/BruteForce/BruteForceOptimizer.cs
+++ QuantProject/b1_ADT/Optimizing/BruteForce/BruteForceOptimizer.cs
@@ -33,28 +33,53 @@ namespace QuantProject.ADT.Optimizing.BruteForce
 	{
 		private IBruteForceOptimizableParametersManager
 			bruteForceOptimizableParametersManager;
+		private int numberOfTopBestParameters;
 
 		private int numberOfAnalizedItemsForNewProgess;
 		private int totalNumberOfItemsToBeAnalized;
 
-		private BruteForceOptimizableParameters bestParameters;
+//		private BruteForceOptimizableParameters bestParameters;
+		private BestParametersManager bestParametersManager;
 
 		private int analizedItems;
+		private bool runIsComplete;
 
 		public event NewProgressEventHandler NewProgress;
 
 		public BruteForceOptimizableParameters BestParameters
 		{
-			get { return this.bestParameters; }
+			get { return this.TopBestParameters[ 0 ]; }
+		}
+		public BruteForceOptimizableParameters[] TopBestParameters
+		{
+			get
+			{
+				if ( !this.runIsComplete )
+					throw new Exception( "This property cannot be invoked until " +
+						"the Run execution is complete!" );
+				return this.bestParametersManager.TopBestParameters;
+			}
 		}
 
+
+		/// <summary>
+		/// Optimizer that enumerates all possible parameter values
+		/// </summary>
+		/// <param name="bruteForceOptimizableParametersManager"></param>
+		/// <param name="numberOfTopBestParameters">number of the best
+		/// optimizable parameters to be returned by the TopBestParameters
+		/// property</param>
 		public BruteForceOptimizer(
-			IBruteForceOptimizableParametersManager bruteForceOptimizableParametersManager )
+			IBruteForceOptimizableParametersManager bruteForceOptimizableParametersManager ,
+			int numberOfTopBestParameters )
 		{
 			this.bruteForceOptimizableParametersManager =
 				bruteForceOptimizableParametersManager;
+			this.numberOfTopBestParameters = numberOfTopBestParameters;
+
 			this.numberOfAnalizedItemsForNewProgess = 100000;
 			this.totalNumberOfItemsToBeAnalized = 0;
+			this.runIsComplete = false;
 		}
 		#region Run
 		private void handleProgress()
@@ -69,24 +94,50 @@ namespace QuantProject.ADT.Optimizing.BruteForce
 					this.NewProgress( this , newProgressEventArgs );
 			}
 		}
+
+		#region Run
+//		private void handleCurrentOptimizableParameters(
+//			BruteForceOptimizableParameters bruteForceOptimizableParameters )
+//		{
+//			if ( !this.topBestParameters.IsFull )
+//				// this.topBestParameters still contains less than
+//				// this.numberOfTopBestParametersToBeReturned items. It means that we are still in
+//				// the initializing phase, when the first this.numberOfTopBestParametersToBeReturned
+//				// items are to be added to the topBestParameters
+//				this.topBestParameters.Analize( bruteForceOptimizableParameters )
+//					else if ( this.topBestParameters.LowestFitness < bruteForceOptimizableParameters.Fitness )
+//				this.updateTopBestParameters( bruteForceOptimizableParameters );
+//		}
+		public void createTopBestParameters()
+		{
+			this.bestParametersManager = new BestParametersManager(
+				this.numberOfTopBestParameters );
+			this.analizedItems = 0;
+			this.bruteForceOptimizableParametersManager.Reset();
+//			this.bestParameters =
+//				(BruteForceOptimizableParameters)this.bruteForceOptimizableParametersManager.Current;
+			while( this.bruteForceOptimizableParametersManager.MoveNext() )
+			{
+				BruteForceOptimizableParameters bruteForceOptimizableParameters =
+					(BruteForceOptimizableParameters)this.bruteForceOptimizableParametersManager.Current;
+				this.bestParametersManager.Analize( bruteForceOptimizableParameters );
+				this.handleProgress();
+			}
+		}
 		/// <summary>
 		/// executes the optimization
 		/// </summary>
 		public void Run()
 		{
-			this.analizedItems = 0;
-			this.bruteForceOptimizableParametersManager.Reset();
-			this.bestParameters =
-				(BruteForceOptimizableParameters)this.bruteForceOptimizableParametersManager.Current;
-			while( this.bruteForceOptimizableParametersManager.MoveNext() )
-			{
-				BruteForceOptimizableParameters bruteForceOptimizableParameters =
-					(BruteForceOptimizableParameters)this.bruteForceOptimizableParametersManager.Current;
-				if ( this.bestParameters.Fitness < bruteForceOptimizableParameters.Fitness )
-					this.bestParameters = bruteForceOptimizableParameters;
-				this.handleProgress();
-			}
+			this.createTopBestParameters();
+//			this.bestParametersManager.TopBestParameters.Sort();
+			this.runIsComplete = true;
 		}
+		#endregion Run
+
+		/// <summary>
+		/// executes the optimization
+		/// </summary>
 		public void Run( int numberOfAnalizedItemsForNewProgess ,
 			int totalNumberOfItemsToBeAnalized )
 		{
