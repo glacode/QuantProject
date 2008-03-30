49be95cb2f567cb566b373a8e972dea2f3a5ed73
glauco_1 <>
2008-03-30 16:00:08 +0000

    CombinationBasedBruteForceOptimizableParametersManager is now inherited (and code is simplified accordingly)

--- QuantProject/b7_Scripts/WalkForwardTesting/WalkForwardLag/WeightedPositionsChoosers/OptimizableParametersManagers/WFLagBruteForceOptimizableParametersManagerForBalancedFixedPortfolioAndBalancedDriving.cs
+++ QuantProject/b7_Scripts/WalkForwardTesting/WalkForwardLag/WeightedPositionsChoosers/OptimizableParametersManagers/WFLagBruteForceOptimizableParametersManagerForBalancedFixedPortfolioAndBalancedDriving.cs
@@ -43,45 +43,45 @@ namespace QuantProject.Scripts.WalkForwardTesting.WalkForwardLag.WeightedPositio
 	/// </summary>
 	public class
 		WFLagBruteForceOptimizableParametersManagerForBalancedFixedPortfolioAndBalancedDriving
-		: IBruteForceOptimizableParametersManager
+		: CombinationBasedBruteForceOptimizableParametersManager
 	{
 //		private double[] weightsForDrivingPositions;
 		private DataTable eligibleTickersForDrivingPositions;
 		private IWFLagDecoder wFLagDecoder;
 		private WFLagFitnessEvaluator wFLagFitnessEvaluator;
 
-		private Combination drivingCombination;
+//		private Combination drivingCombination;
 
 //		private double[] standardDeviationForDrivingPositions;
 //		private float[][] drivingPositionsCloseToCloseReturns;
 
-		public object Current
-		{
-			get
-			{
-				int[] currentValues = new int[ this.drivingCombination.Length ];
-				for ( int i = 0 ; i < this.drivingCombination.Length ; i ++ )
-					currentValues[ i ] = this.drivingCombination.GetValue( i );
-//				for ( int i = this.drivingCombination.Length ;
-//					i < this.drivingCombination.Length +
-//					this.portfolioCombination.Length ; i ++ )
-//					currentValues[ i ] =
-//						this.portfolioCombination.GetValue( i - this.drivingCombination.Length );
-				BruteForceOptimizableParameters bruteForceOptimizableParameters =
-					new BruteForceOptimizableParameters( currentValues ,
-					this );
-				return bruteForceOptimizableParameters;
-			}
-		}
-
-
-		public int TotalIterations
-		{
-			get
-			{
-				return Convert.ToInt32( this.drivingCombination.TotalNumberOfCombinations );
-			}
-		}
+//		public object Current
+//		{
+//			get
+//			{
+//				int[] currentValues = new int[ this.drivingCombination.Length ];
+//				for ( int i = 0 ; i < this.drivingCombination.Length ; i ++ )
+//					currentValues[ i ] = this.drivingCombination.GetValue( i );
+////				for ( int i = this.drivingCombination.Length ;
+////					i < this.drivingCombination.Length +
+////					this.portfolioCombination.Length ; i ++ )
+////					currentValues[ i ] =
+////						this.portfolioCombination.GetValue( i - this.drivingCombination.Length );
+//				BruteForceOptimizableParameters bruteForceOptimizableParameters =
+//					new BruteForceOptimizableParameters( currentValues ,
+//					this );
+//				return bruteForceOptimizableParameters;
+//			}
+//		}
+//
+//
+//		public int TotalIterations
+//		{
+//			get
+//			{
+//				return Convert.ToInt32( this.drivingCombination.TotalNumberOfCombinations );
+//			}
+//		}
 
 		public WFLagBruteForceOptimizableParametersManagerForBalancedFixedPortfolioAndBalancedDriving(
 			DataTable eligibleTickersForDrivingPositions ,
@@ -92,7 +92,11 @@ namespace QuantProject.Scripts.WalkForwardTesting.WalkForwardLag.WeightedPositio
 			int numberOfDrivingPositions ,
 			IWFLagDecoder wFLagDecoder ,
 			IEquityEvaluator equityEvaluator ,
-			ReturnsManager returnsManager )
+			ReturnsManager returnsManager ) :
+			base( new Combination(
+			- eligibleTickersForDrivingPositions.Rows.Count ,
+			eligibleTickersForDrivingPositions.Rows.Count - 1 ,
+			numberOfDrivingPositions ) )
 		{
 			this.eligibleTickersForDrivingPositions =
 				eligibleTickersForDrivingPositions;
@@ -100,156 +104,28 @@ namespace QuantProject.Scripts.WalkForwardTesting.WalkForwardLag.WeightedPositio
 			this.wFLagFitnessEvaluator =
 				new WFLagFitnessEvaluator( equityEvaluator ,
 				returnsManager );
-			this.drivingCombination = new Combination(
-				- eligibleTickersForDrivingPositions.Rows.Count ,
-				eligibleTickersForDrivingPositions.Rows.Count - 1 ,
-				numberOfDrivingPositions );
-		}
-//		protected override double[]
-//			getWeightRelatedParameterValuesForDrivingPositions(
-//			int[] optimizableParameters )
-//		{
-//			double[] weightsForPortfolioPositions =
-//				this.getWeightsForPortfolioPositions();
-//			string[] tickersForPortfolioPositions =
-//				new string[ 2 ] { this.portfolioLongTicker , this.portfolioShortTicker };
-//			WeightedPositions weightedPositions =
-//				new WeightedPositions( weightsForPortfolioPositions ,
-//				tickersForPortfolioPositions );
-//			return weightedPositions;
-//		}
-//		#region getWeightRelatedParameterValuesForDrivingPositions
-//		private ArrayList getArrayListOfEligibleTickersForDrivingPositions()
-//		{
-//			ArrayList tickers = new ArrayList();
-//			foreach ( DataRow eligibleDrivingTicker in
-//				this.eligibleTickersForDrivingWeightedPositions.Rows )
-//				tickers.Add( eligibleDrivingTicker[ 0 ] );
-//			return tickers;
-//		}
-//		private void setDrivingPositionsCloseToCloseReturns()
-//		{
-//			ArrayList tickers =
-//				this.getArrayListOfEligibleTickersForDrivingPositions();
-//			this.drivingPositionsCloseToCloseReturns =
-//				this.wFLagCandidates.GetTickersReturns( tickers );
-//		}
-//		private void setStandardDeviationForDrivingPositionsActually(
-//			int drivingPositionIndex )
-//		{
-//			this.standardDeviationForDrivingPositions[ drivingPositionIndex ] =
-//				BasicFunctions.GetStdDev(
-//				this.drivingPositionsCloseToCloseReturns[ drivingPositionIndex ] );
-//		}
-//		private void setStandardDeviationForDrivingPositionsActually()
-//		{
-//			this.setDrivingPositionsCloseToCloseReturns();
-//			this.standardDeviationForDrivingPositions =
-//				new double[ this.eligibleTickersForDrivingWeightedPositions.Rows.Count ];
-//			for ( int i = 0 ;
-//				i < this.eligibleTickersForDrivingWeightedPositions.Rows.Count ;
-//				i++ )
-//				this.setStandardDeviationForDrivingPositionsActually( i );
-//		}
-//		private void setStandardDeviationForDrivingPositions()
-//		{
-//			if ( this.standardDeviationForDrivingPositions == null )
-//				// this.standardDeviationForDrivingPositions has not been set yet
-//				this.setStandardDeviationForDrivingPositionsActually();
-//		}
-//		private double getStandardDeviation( int[] parameterValues ,
-//			int parameterPosition )
-//		{
-//			int tickerIndex =	this.getTickerIndexForDrivingPosition(
-//				parameterValues , parameterPosition );
-//			return this.standardDeviationForDrivingPositions[ tickerIndex ];
-//		}
-//		private double getMaxStandardDeviationForCurrentDrivingPositions(
-//			int[] optimizableParameters )
-//		{
-//			double maxStandardDeviationForCurrentDrivingPositions = 0;
-//			for ( int parameterPosition = 0 ; parameterPosition < optimizableParameters.Length ;
-//				parameterPosition++ )
-//			{
-//				double drivingPositionStandardDeviation =
-//					this.getStandardDeviation( optimizableParameters , parameterPosition );
-//				if ( drivingPositionStandardDeviation > maxStandardDeviationForCurrentDrivingPositions )
-//					maxStandardDeviationForCurrentDrivingPositions = drivingPositionStandardDeviation;
-//			}
-//			return maxStandardDeviationForCurrentDrivingPositions;
-//		}
-//		private double getNonNormalizedWeightForDrivingPosition(
-//			int[] parameterValues ,
-//			int parameterPosition ,
-//			double maxStandardDeviationForCurrentDrivingPositions )
-//		{
-//			double drivingPositionStandardDeviation =
-//				this.getStandardDeviation( parameterValues , parameterPosition );
-//			double nonNormalizedWeightForDrivingPosition =
-//				maxStandardDeviationForCurrentDrivingPositions /
-//				drivingPositionStandardDeviation;
-//			if ( parameterValues[ parameterPosition ] < 0 )
-//				nonNormalizedWeightForDrivingPosition =
-//					-nonNormalizedWeightForDrivingPosition;
-//			return nonNormalizedWeightForDrivingPosition;
-//		}
-//		private double[] getNonNormalizedWeightsForDrivingPositionsUsingStandardDeviations(
-//			int[] parameterValues ,
-//			double maxStandardDeviationForCurrentDrivingPositions )
-//		{
-//			double[] nonNormalizedWeightsForDrivingPositions =
-//				new double[ parameterValues.Length ];
-//			for ( int parameterPosition = 0 ; parameterPosition < parameterValues.Length ;
-//				parameterPosition++ )
-//			{
-//				nonNormalizedWeightsForDrivingPositions[ parameterPosition ] =
-//					this.getNonNormalizedWeightForDrivingPosition(
-//					parameterValues , parameterPosition ,
-//					maxStandardDeviationForCurrentDrivingPositions );
-//			}
-//			return nonNormalizedWeightsForDrivingPositions;
-//		}
-//		private double[] getNonNormalizedWeightsForDrivingPositions(
-//			int[] parameterValues )
-//		{
-//			double maxStandardDeviationForCurrentDrivingPositions =
-//				this.getMaxStandardDeviationForCurrentDrivingPositions( parameterValues );
-//			return getNonNormalizedWeightsForDrivingPositionsUsingStandardDeviations(
-//				parameterValues , maxStandardDeviationForCurrentDrivingPositions );
-//		}
-//		private double[] getWeightsForDrivingPositions(
-//			int[] parameterValues )
-//		{
-//			double[] nonNormalizedWeightsForDrivingPositions =
-//				this.getNonNormalizedWeightsForDrivingPositions( parameterValues );
-//			return
-//				WeightedPositions.GetNormalizedWeights(
-//				nonNormalizedWeightsForDrivingPositions );
-//		}
-//		protected override double[]
-//			getWeightRelatedParameterValuesForDrivingPositions(
-//			int[] parameterValues )
-//		{
-//			this.setStandardDeviationForDrivingPositions();
-//			return this.getWeightsForDrivingPositions( parameterValues );
-//		}
-//		#endregion
-		public bool MoveNext()
-		{
-			return this.drivingCombination.MoveNext();
-		}
-		public void Reset()
-		{
-			this.drivingCombination.Reset();
+//			this.drivingCombination = new Combination(
+//				- eligibleTickersForDrivingPositions.Rows.Count ,
+//				eligibleTickersForDrivingPositions.Rows.Count - 1 ,
+//				numberOfDrivingPositions );
 		}
 
-		public object Decode( BruteForceOptimizableParameters
+//		public bool MoveNext()
+//		{
+//			return this.drivingCombination.MoveNext();
+//		}
+//		public void Reset()
+//		{
+//			this.drivingCombination.Reset();
+//		}
+
+		public override object Decode( BruteForceOptimizableParameters
 			bruteForceOptimizableParameters )
 		{
 			return this.wFLagDecoder.Decode(
 				bruteForceOptimizableParameters.GetValues() );
 		}
-		public double GetFitnessValue(
+		public override double GetFitnessValue(
 			BruteForceOptimizableParameters bruteForceOptimizableParameters )
 		{
 			object meaning = this.Decode(
