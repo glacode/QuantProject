fc0b2cd4771d5af400cc76fb9cd85bd96991c416
glauco_1 <>
2008-08-16 19:09:59 +0000

    Decoder for the Fixed Length Two Phases strategy

---
+++ QuantProject/b7_Scripts/WalkForwardTesting/FixedLengthTwoPhases/InSampleChoosers/Decoding/DecoderForFLTPTestingPositionsWithBalancedWeights.cs
@@ -0,0 +1,55 @@
+/*
+QuantProject - Quantitative Finance Library
+
+DecoderForFLTPTestingPositionsWithBalancedWeights.cs
+Copyright (C) 2008
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+using System;
+
+using QuantProject.Business.Strategies;
+using QuantProject.Business.Strategies.Optimizing.Decoding;
+using QuantProject.Business.Strategies.OutOfSample;
+
+namespace QuantProject.Scripts.WalkForwardTesting.FixedLengthTwoPhases
+{
+	/// <summary>
+	/// Decoder for the Fixed Length Two Phases strategy
+	/// </summary>
+	public class DecoderForFLTPTestingPositionsWithBalancedWeights :
+		DecoderForTestingPositionsWithBalancedWeights
+	{
+		public DecoderForFLTPTestingPositionsWithBalancedWeights()
+		{
+		}
+
+		protected override TestingPositions getMeaningForUndecodable()
+		{
+			return new FLTPTestingPositions();
+		}
+		
+		protected override TestingPositions getTestingPositions(double[] weights, string[] tickers)
+		{
+			WeightedPositions weightedPositions =
+				new WeightedPositions( weights , tickers );
+			FLTPTestingPositions fLTPTestingPositions =
+				new FLTPTestingPositions( weightedPositions );
+			return fLTPTestingPositions;
+		}
+	}
+}
