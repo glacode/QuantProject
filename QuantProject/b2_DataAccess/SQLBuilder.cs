e9ffbd7ad19c7847917b987fd145ae2f59c8a6be
glauco_1 <>
2008-11-08 20:31:17 +0000

    the following method has been added internal static string GetDateTimeConstant( DateTime dateTime )
    the following method has been added
    public static string GetTimeConstant( DateTime dateTime )
    
    the following method has been added
    internal static string GetFilterForTime( string fieldName , SqlComparisonOperator sqlComparisonOperator , DateTime time )

--- QuantProject/b2_DataAccess/SQLBuilder.cs
+++ QuantProject/b2_DataAccess/SQLBuilder.cs
@@ -2,7 +2,7 @@
 QuantProject - Quantitative Finance Library
 
 SQLBuilder.cs
-Copyright (C) 2003 
+Copyright (C) 2003
 Glauco Siliprandi
 
 This program is free software; you can redistribute it and/or
@@ -18,9 +18,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
+ */
 
 using System;
+
+using QuantProject.ADT;
 
 namespace QuantProject.DataAccess
 {
@@ -36,12 +38,116 @@ namespace QuantProject.DataAccess
 			//
 		}
 
-    internal static string GetDateConstant( DateTime dateTime )
-    {
-      string getDateConstant;
-      getDateConstant = "#" + dateTime.Month + "/" + dateTime.Day + "/" +
-        dateTime.Year + "#";
-      return getDateConstant;
-    }
+		internal static string GetDateConstant( DateTime dateTime )
+		{
+			string getDateConstant;
+			getDateConstant = "#" + dateTime.Month + "/" + dateTime.Day + "/" +
+				dateTime.Year + "#";
+			return getDateConstant;
+		}
+
+		/// <summary>
+		/// returns a string to be used as a DateTime constant for a query
+		/// for an Access database
+		/// </summary>
+		/// <param name="dateTime"></param>
+		/// <returns></returns>
+		internal static string GetDateTimeConstant( DateTime dateTime )
+		{
+			string dateTimeConstant =
+				"#" +
+				dateTime.Month + "/" +
+				dateTime.Day + "/" +
+				dateTime.Year + " " +
+				dateTime.Hour + ":" +
+				dateTime.Minute + ":" +
+				dateTime.Second +
+				"#";
+			return dateTimeConstant;
+		}
+		
+		#region GetTimeConstant
+		public static string GetTimeConstant( DateTime dateTime )
+		{
+			string timeConstant =
+				"'" +
+				dateTime.Hour.ToString( "00" ) + "." +
+				dateTime.Minute.ToString( "00" ) + "." +
+				dateTime.Second.ToString( "00" ) + "'";
+			return timeConstant;
+		}
+		#endregion GetTimeConstant
+		
+		#region GetFilterForTime
+		private static void getFilterForTime_checkParameters( DateTime time )
+		{
+//			if ( ( comparisonOperator != "=" ) &&
+//			    ( comparisonOperator != "<" ) &&
+//			    ( comparisonOperator != "<=" ) &&
+//			    ( comparisonOperator != ">" ) &&
+//			    ( comparisonOperator != ">=" ) )
+//				throw new Exception(
+//					"comparisonOperator can either be '=' or '<' or '<=' or '>' or '>='" );
+			if ( !ExtendedDateTime.IsTime( time ) )
+				throw new Exception(
+					"time is actually not a time. Use the method " +
+					"QuantProject.ADT.ExtendedDateTime.GetTime() to build your time" );
+		}
+		
+		#region getFilterForTime_actually
+		private static string getSqlStringForComparisonOperator(
+			SqlComparisonOperator sqlComparisonOperator )
+		{
+			string sqlStringForComparisonOperator = "";
+			switch( sqlComparisonOperator )
+			{
+				case SqlComparisonOperator.Equal:
+					sqlStringForComparisonOperator = "=";
+					break;
+				case SqlComparisonOperator.LessThan:
+					sqlStringForComparisonOperator = "<";
+					break;
+				case SqlComparisonOperator.LessThanOrEqual:
+					sqlStringForComparisonOperator = "<=";
+					break;
+				case SqlComparisonOperator.GreaterThan:
+					sqlStringForComparisonOperator = ">";
+					break;
+				case SqlComparisonOperator.GreaterThanOrEqual:
+					sqlStringForComparisonOperator = ">=";
+					break;
+			}
+			return sqlStringForComparisonOperator;
+		}
+		private static string getFilterForTime_actually(
+			string fieldName , SqlComparisonOperator sqlComparisonOperator , DateTime time )
+		{
+			string filterForDailyTime =
+				"(Format([baDateTimeForOpen],'hh:mm:ss')" +
+				SQLBuilder.getSqlStringForComparisonOperator( sqlComparisonOperator ) +
+				SQLBuilder.GetTimeConstant( time );
+			return filterForDailyTime;
+		}
+		#endregion getFilterForTime_actually
+		
+		/// <summary>
+		/// returns a sql where expression, to compare a field value with
+		/// the given time
+		/// </summary>
+		/// <param name="fieldName"></param>
+		/// <param name="comparisonOperator">it can either be "=" or "<" or
+		/// "<=" or ">" or ">="</param>
+		/// <param name="dateTime"></param>
+		/// <returns></returns>
+		internal static string GetFilterForTime(
+			string fieldName , SqlComparisonOperator sqlComparisonOperator , DateTime time )
+		{
+			SQLBuilder.getFilterForTime_checkParameters( time );
+			string filterForTime =
+				SQLBuilder.getFilterForTime_actually(
+					fieldName , sqlComparisonOperator , time );
+			return filterForTime;
+		}
+		#endregion GetFilterForTime
 	}
 }
