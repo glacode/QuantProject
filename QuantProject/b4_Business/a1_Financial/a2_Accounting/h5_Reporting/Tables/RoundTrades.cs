05ae065a980328b11ffec12deef1386cf8eedf22
millo_1 <>
2009-03-29 18:15:50 +0000

    The table RoundTrades in report has been fixed. Old reports are still deserializable.

--- QuantProject/b4_Business/a1_Financial/a2_Accounting/h5_Reporting/Tables/RoundTrades.cs
+++ QuantProject/b4_Business/a1_Financial/a2_Accounting/h5_Reporting/Tables/RoundTrades.cs
@@ -11,17 +11,32 @@ namespace QuantProject.Business.Financial.Accounting.Reporting.Tables
   public class RoundTrades : ReportTable
 	{
     private ReportTable transactionTable;
-
+    private string fieldNameForTicker;
+		private string fieldNameForDateTime;
+		private string fieldNameForTransactionType;
+		
+		private void setFieldNames()
+		{
+			this.fieldNameForTicker = 
+				QuantProject.Business.Financial.Accounting.Reporting.Tables.Transactions.FieldNameForTicker;
+			this.fieldNameForDateTime = 
+				QuantProject.Business.Financial.Accounting.Reporting.Tables.Transactions.FieldNameForDateTime;
+			this.fieldNameForTransactionType = 
+				QuantProject.Business.Financial.Accounting.Reporting.Tables.Transactions.FieldNameForTransactionType;
+		}
     public RoundTrades( string reportName , ReportTable transactionTable ) :
       base( reportName + " - Round Trades" )
 		{
       this.transactionTable = transactionTable;
+      this.setFieldNames();
       this.DataTable = this.getRoundTrades();
     }
     #region "getRoundTrades"
     private void getRoundTradeTable_setColumns( DataTable roundTradeDataTable )
     {
+      roundTradeDataTable.Columns.Add( fieldNameForTicker , Type.GetType( "System.String" ) );
       roundTradeDataTable.Columns.Add( "Trade" , Type.GetType( "System.String" ) );
+      roundTradeDataTable.Columns.Add( "Quantity"  , Type.GetType( "System.Int32" ) );
       roundTradeDataTable.Columns.Add( "EntryDate"  , Type.GetType( "System.DateTime" ) );
       roundTradeDataTable.Columns.Add( "EntryPrice" , Type.GetType( "System.Double" ) );
       roundTradeDataTable.Columns.Add( "ExitDate"  , Type.GetType( "System.DateTime" ) );
@@ -29,61 +44,72 @@ namespace QuantProject.Business.Financial.Accounting.Reporting.Tables
       roundTradeDataTable.Columns.Add( "%chg"  , Type.GetType( "System.Double" ) );
       roundTradeDataTable.Columns.Add( "Profit"  , Type.GetType( "System.Double" ) );
       roundTradeDataTable.Columns.Add( "%Profit"  , Type.GetType( "System.Double" ) );
-      roundTradeDataTable.Columns.Add( "#bars"  , Type.GetType( "System.Int32" ) );
-      roundTradeDataTable.Columns.Add( "ProfitPerBar"  , Type.GetType( "System.Double" ) );
+      roundTradeDataTable.Columns.Add( "#minutes"  , Type.GetType( "System.Int32" ) );
+      roundTradeDataTable.Columns.Add( "ProfitPerMinute"  , Type.GetType( "System.Double" ) );
       //roundTradeDataTable.Columns.Add( "AccountValue"  , Type.GetType( "System.Double" ) );
       //roundTradeDataTable.Columns.Add( "PnL"  , Type.GetType( "System.Double" ) );
     }
-    private void getRoundTradeTable_setRow( DataRow dataRow , DataTable roundTradeDataTable )
+    private void getRoundTradeTable_addRow( int rowIndex, DataRow dataRow , DataTable roundTradeDataTable )
     {
-      DataRow roundTrade;
-      switch ( (string)dataRow[ "TransactionType" ] )
-      {
-        case "BuyLong":
-          roundTrade = roundTradeDataTable.NewRow();
-          roundTrade[ "Trade" ] = "Long";
-          roundTrade[ "EntryDate" ] = dataRow[ "DateTime" ];
-          roundTrade[ "EntryPrice" ] = dataRow[ "Price" ];
-          roundTradeDataTable.Rows.Add( roundTrade );
-          break;
-        case "Sell":
-          roundTrade = roundTradeDataTable.Rows[ roundTradeDataTable.Rows.Count - 1 ];
-          roundTrade[ "ExitDate" ] = dataRow[ "DateTime" ];
-          roundTrade[ "ExitPrice" ] = dataRow[ "Price" ];
-          roundTrade[ "%chg" ] =
-            ((double)roundTrade[ "ExitPrice" ] - (double)roundTrade[ "EntryPrice" ])/
-            ((double)roundTrade[ "EntryPrice" ])*100;
-          roundTrade[ "%Profit" ] = roundTrade[ "%chg" ];
-          roundTrade[ "#bars" ] =
-            ((TimeSpan)((DateTime)roundTrade[ "ExitDate" ] - (DateTime)roundTrade[ "EntryDate" ])).Days;
-          roundTrade[ "ProfitPerBar" ] = (double)roundTrade[ "%chg" ] / (int)roundTrade[ "#bars" ];
-          break;
-        case "SellShort":
-          roundTrade = roundTradeDataTable.NewRow();
-          roundTrade[ "Trade" ] = "Short";
-          roundTrade[ "EntryDate" ] = dataRow[ "DateTime" ];
-          roundTrade[ "EntryPrice" ] = dataRow[ "Price" ];
-          roundTradeDataTable.Rows.Add( roundTrade );
-          break;
-        case "Cover":
-          roundTrade = roundTradeDataTable.Rows[ roundTradeDataTable.Rows.Count - 1 ];
-          roundTrade[ "ExitDate" ] = dataRow[ "DateTime" ];
-          roundTrade[ "ExitPrice" ] = dataRow[ "Price" ];
-          roundTrade[ "%chg" ] =
-            ((double)roundTrade[ "ExitPrice" ] - (double)roundTrade[ "EntryPrice" ])/
-            ((double)roundTrade[ "EntryPrice" ])*100;
-          roundTrade[ "%Profit" ] = - ((double)roundTrade[ "%chg" ]);
-          roundTrade[ "#bars" ] =
-            ((TimeSpan)((DateTime)roundTrade[ "ExitDate" ] - (DateTime)roundTrade[ "EntryDate" ])).Days;
-          roundTrade[ "ProfitPerBar" ] = (double)roundTrade[ "%chg" ] / (int)roundTrade[ "#bars" ];
-          break;
-      }
-
+			DateTime startDateTimeOfTrade = (DateTime)dataRow[fieldNameForDateTime];
+			DateTime endDateTimeOfTrade = new DateTime(1900,1,1);
+			string currentTicker = (string)dataRow[fieldNameForTicker];
+			string currentTradeType = (string)dataRow[fieldNameForTransactionType];
+			double signForRoundTradeProfit = 1.0;
+    	if(currentTradeType == "SellShort")
+    		signForRoundTradeProfit = -1.0;
+			double entryPrice = (double)dataRow["Price"];
+			double exitPrice = 0.0;
+			int instrumentQuantity = (int)dataRow["Quantity"];
+			for (int j = rowIndex + 1; j < this.transactionTable.DataTable.Rows.Count ; j++)
+			{
+				if( currentTicker == (string)this.transactionTable.DataTable.Rows[ j ][fieldNameForTicker] &&
+				    ((currentTradeType == "BuyLong" && 
+				     (string)this.transactionTable.DataTable.Rows[ j ][fieldNameForTransactionType] == "Sell")
+				     ||
+				     (currentTradeType == "SellShort" && 
+				     (string)this.transactionTable.DataTable.Rows[ j ][fieldNameForTransactionType] == "Cover")
+				    ) )
+				{
+					endDateTimeOfTrade = 
+						(DateTime)this.transactionTable.DataTable.Rows[ j ][fieldNameForDateTime];
+					exitPrice = (double)this.transactionTable.DataTable.Rows[ j ]["Price"];
+					j = this.transactionTable.DataTable.Rows.Count;
+				}
+			}
+			TimeSpan timeSpanBetweenEnterAndExit = 
+				endDateTimeOfTrade.Subtract(startDateTimeOfTrade);
+      DataRow roundTrade = roundTradeDataTable.NewRow();
+      roundTrade[ fieldNameForTicker ] = currentTicker;
+      roundTrade[ "Trade" ] = currentTradeType;
+      roundTrade[ "Quantity" ] = instrumentQuantity;
+      roundTrade[ "EntryDate" ] = startDateTimeOfTrade;
+      roundTrade[ "ExitDate" ] = endDateTimeOfTrade;
+      roundTrade[ "EntryPrice" ] = entryPrice;
+      roundTrade[ "ExitPrice" ] = exitPrice;
+      roundTrade[ "%chg" ] = (exitPrice - entryPrice)/entryPrice * 100.0;
+      roundTrade[ "%Profit" ] = signForRoundTradeProfit * (double)roundTrade[ "%chg" ];
+      roundTrade[ "Profit" ] = signForRoundTradeProfit * instrumentQuantity * (exitPrice - entryPrice);
+      roundTrade[ "#minutes" ] = timeSpanBetweenEnterAndExit.TotalMinutes;
+      roundTrade[ "ProfitPerMinute" ] = (double)roundTrade[ "%Profit" ] / (int)roundTrade[ "#minutes" ];
+      roundTradeDataTable.Rows.Add(roundTrade);
     }
+    private bool doesCurrentRowIndexPointToABuyLongOrASellShort( int rowIndex )
+		{
+			bool returnValue;
+			string currentTransactionType =
+				(string)this.transactionTable.DataTable.Rows[ rowIndex ][fieldNameForTransactionType];
+			returnValue =
+				( currentTransactionType == "BuyLong" || currentTransactionType == "SellShort" );
+			return returnValue;
+		}
     private void getRoundTradeTable_setRows( DataTable roundTradeDataTable )
     {
-      foreach (DataRow dataRow in this.transactionTable.DataTable.Rows )
-        getRoundTradeTable_setRow( dataRow , roundTradeDataTable );
+      for(int rowIndex = 0; rowIndex < this.transactionTable.DataTable.Rows.Count; rowIndex++ )
+      	if( this.doesCurrentRowIndexPointToABuyLongOrASellShort( rowIndex ) )
+					getRoundTradeTable_addRow( rowIndex,
+      	                           	 transactionTable.DataTable.Rows[rowIndex],
+      	                             roundTradeDataTable );
     }
     private DataTable getRoundTrades()
     {
@@ -93,6 +119,5 @@ namespace QuantProject.Business.Financial.Accounting.Reporting.Tables
       return roundTradeDataTable;
     }
     #endregion
-
 	}
 }
