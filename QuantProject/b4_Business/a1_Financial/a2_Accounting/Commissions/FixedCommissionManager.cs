36fa8b79e50833a16c403695102dc92c4e0b8fe5
millo_1 <>
2009-08-31 20:56:30 +0000

    Added FixedCommission and FixedCommissionManager, for management of fixed commissions

---
+++ QuantProject/b4_Business/a1_Financial/a2_Accounting/Commissions/FixedCommissionManager.cs
@@ -0,0 +1,45 @@
+/*
+QuantProject - Quantitative Finance Library
+
+FixedCommissionManager.cs
+Copyright (C) 2009 
+Marco Milletti
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+using System;
+
+namespace QuantProject.Business.Financial.Accounting.Commissions
+{
+	/// <summary>
+	/// Commission Manager for fixed value commissions.
+	/// </summary>
+	[Serializable]
+  public class FixedCommissionManager : ICommissionManager
+	{
+		double fixedCommission;
+  	public FixedCommissionManager(double fixedCommission)
+		{
+  		this.fixedCommission = fixedCommission;
+		}
+		public Commission GetCommission( Transaction transaction )
+		{
+			return new FixedCommission( transaction, this.fixedCommission );
+		}
+	}
+}
+
+
