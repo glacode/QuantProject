a2c7ecbaf19e80bde70b53c3bae6f92f3f860dfe
glauco_1 <>
2010-01-31 19:14:58 +0000

    The license statement has been added

--- QuantProject/t5_Testing/b1_ADT/Sum.cs
+++ QuantProject/t5_Testing/b1_ADT/Sum.cs
@@ -1,11 +1,24 @@
 /*
- * Created by SharpDevelop.
- * User: Glauco
- * Date: 1/31/2010
- * Time: 7:26 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
+QuantProject - Quantitative Finance Library
+
+Sum.cs
+Copyright (C) 2003
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 
 using System;
 using NUnit.Framework;
