3792894cfa1cab2b5339405c62c09156c88cd26b
glauco_1 <>
2010-03-27 17:31:33 +0000

    The code has been reformatted, and it now follows a standard indentation

--- QuantProject/b3_Data/Selectors/SelectorByGroup.cs
+++ QuantProject/b3_Data/Selectors/SelectorByGroup.cs
@@ -2,7 +2,7 @@
 QuantProject - Quantitative Finance Library
 
 SelectorByGroup.cs
-Copyright (C) 2003 
+Copyright (C) 2003
 Glauco Siliprandi
 
 This program is free software; you can redistribute it and/or
@@ -18,7 +18,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
+ */
 
 using System;
 using System.Collections;
@@ -32,46 +32,45 @@ namespace QuantProject.Data.Selectors
 	/// <summary>
 	/// Class for selection on tickers by groupId only or groupId and date
 	/// </summary>
-	public class SelectorByGroup : ITickerSelector 
+	public class SelectorByGroup : ITickerSelector
 	{
-    private string groupID;
-    private DateTime date = new DateTime(1900,1,1);
-    
-    /// <summary>
-    /// Creates an new instance of SelectorByGroup, in order
-    /// to get tickers contained in the given group
-    /// </summary>
-    /// <param name="groupID">Group's code for which tickers are to be selected</param>
+		private string groupID;
+		private DateTime date = new DateTime(1900,1,1);
+		
+		/// <summary>
+		/// Creates an new instance of SelectorByGroup, in order
+		/// to get tickers contained in the given group
+		/// </summary>
+		/// <param name="groupID">Group's code for which tickers are to be selected</param>
 		public SelectorByGroup( string groupID )
 		{
 			this.groupID = groupID;
-   	}
-    
-    /// <summary>
-    /// Creates an new instance of SelectorByGroup, in order
-    /// to get tickers contained in the given group
-    /// </summary>
-    /// <param name="groupID">Group's code for which tickers are to be selected</param>
-    /// <param name="date">The date at which tickers to be selected belong effectively
-    ///                           to the given group</param>
-    public SelectorByGroup( string groupID, DateTime date )
-    {
-      this.groupID = groupID;
-      this.date = date;
-    }
+		}
+		
+		/// <summary>
+		/// Creates an new instance of SelectorByGroup, in order
+		/// to get tickers contained in the given group
+		/// </summary>
+		/// <param name="groupID">Group's code for which tickers are to be selected</param>
+		/// <param name="date">The date at which tickers to be selected belong effectively
+		///                           to the given group</param>
+		public SelectorByGroup( string groupID, DateTime date )
+		{
+			this.groupID = groupID;
+			this.date = date;
+		}
 
 		public DataTable GetTableOfSelectedTickers()
 		{
 			if(this.date.CompareTo(new DateTime(1900,1,1)) == 0)
-      //date has not be set by the user because it is still equal to default value
-        return QuantProject.DataAccess.Tables.Tickers_tickerGroups.GetTickers( this.groupID );
-      else
-        return QuantProject.DataAccess.Tables.Tickers_tickerGroups.GetTickers( this.groupID, this.date );
+				//date has not be set by the user because it is still equal to default value
+				return QuantProject.DataAccess.Tables.Tickers_tickerGroups.GetTickers( this.groupID );
+			else
+				return QuantProject.DataAccess.Tables.Tickers_tickerGroups.GetTickers( this.groupID, this.date );
 		}
 		public void SelectAllTickers()
 		{
 			;
-		}	
-
+		}
 	}
 }
