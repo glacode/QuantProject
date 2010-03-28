d2849027bd895f8440947c420a45c046b44a8f3b
glauco_1 <>
2010-03-28 14:10:07 +0000

    The following public method has been added:
    DateTime GetThisOrNextDateTimeWhenTraded(
    	DateTime startingDateTime ,
    	Time dailyTime ,
    	DateTime lastAcceptableDateTime )
    
    Starting from startingDateTime, this method searches for the next dateTime when the benchmark is traded at time dailyTime.
    The returned value is greater than or equal to startingDateTime, that is, startingDateTime itself is a valid candidate.
    If no solution is found within maxNumberOfDaysToLookAheadFor days, then an exception is returned

--- QuantProject/b4_Business/a2_Strategies/Benchmark.cs
+++ QuantProject/b4_Business/a2_Strategies/Benchmark.cs
@@ -2,6 +2,7 @@ using System;
 
 using QuantProject.ADT;
 using QuantProject.ADT.Histories;
+using QuantProject.ADT.Timing;
 using QuantProject.Business.DataProviders;
 using QuantProject.Business.Strategies.ReturnsManagement.Time;
 using QuantProject.Business.Timing;
@@ -16,7 +17,7 @@ namespace QuantProject.Business.Strategies
 	public class Benchmark
 	{
 		private string ticker;
-		private HistoricalMarketValueProvider historicalQuoteProvider;
+		private HistoricalMarketValueProvider historicalMarketValueProvider;
 		
 		public string Ticker
 		{
@@ -27,33 +28,41 @@ namespace QuantProject.Business.Strategies
 		{
 			this.ticker = ticker;
 
-			this.historicalQuoteProvider = new HistoricalAdjustedQuoteProvider();
+			this.historicalMarketValueProvider = new HistoricalAdjustedQuoteProvider();
+		}
+
+		public Benchmark(
+			string ticker , HistoricalMarketValueProvider historicalMarketValueProvider )
+		{
+			this.ticker = ticker;
+
+			this.historicalMarketValueProvider = historicalMarketValueProvider;
 		}
 
 		#region GetTimeStep
 		private void getTimeStep_checkParameters(
 			DateTime timeStepBegin )
 		{
-			if ( !this.historicalQuoteProvider.WasExchanged( this.ticker ,
-				timeStepBegin ) )
+			if ( !this.historicalMarketValueProvider.WasExchanged( this.ticker ,
+			                                                      timeStepBegin ) )
 				throw new Exception( "The benchmark '" + this.ticker +
-					"' was not exchanged at the given timeStepBegin " +
-					timeStepBegin.ToString() );
+				                    "' was not exchanged at the given timeStepBegin " +
+				                    timeStepBegin.ToString() );
 		}
 		private void isExchanged_checkParameter(
 			DateTime dateTime )
 		{
 			if ( dateTime.Year >
-				DateTime.Now.Year )
+			    DateTime.Now.Year )
 				throw new Exception( "It looks like this is a loop! " +
-					"You have probably asked for a GetTimeStep, but " +
-					"there is no other trading day for this benchmark: '" +
-					this.ticker + "'" );
+				                    "You have probably asked for a GetTimeStep, but " +
+				                    "there is no other trading day for this benchmark: '" +
+				                    this.ticker + "'" );
 		}
 		private bool isExchanged( DateTime dateTime )
 		{
 			this.isExchanged_checkParameter( dateTime );
-			bool isTraded =	this.historicalQuoteProvider.WasExchanged(
+			bool isTraded =	this.historicalMarketValueProvider.WasExchanged(
 				this.ticker , dateTime );
 			return isTraded;
 		}
@@ -88,7 +97,7 @@ namespace QuantProject.Business.Strategies
 
 		/// <summary>
 		/// Returns either the next market close or the next market open
-		/// (when the benchmark is exchanged), whichever is the nearest. 
+		/// (when the benchmark is exchanged), whichever is the nearest.
 		/// If dateTime is either a market close or a market open
 		/// when the benchmark is exchanged, then dateTime itself
 		/// is returned
@@ -121,7 +130,7 @@ namespace QuantProject.Business.Strategies
 		{
 			if( lastDateTime <=	firstDateTime )
 				throw new Exception("lastDateTime has to be greater than " +
-					"firstEndOfDayDateTime !!");
+				                    "firstEndOfDayDateTime !!");
 			History endOfDayHistory = new History();
 			DateTime dateTimeToAddToHistory =
 				ExtendedDateTime.Copy( firstDateTime );
@@ -138,6 +147,73 @@ namespace QuantProject.Business.Strategies
 			}
 			return endOfDayHistory;
 		}
+		
+		#region GetThisOrNextDateTimeWhenTraded
+		private DateTime getDateTimeCandidate(
+			DateTime minDateTimeToTry , Time dailyTime )
+		{
+			Date dateUsedToBuildDateTimeCandidate = new Date( minDateTimeToTry );
+			Time timeForMinDateTimeToTry = new Time( minDateTimeToTry );
+			if ( timeForMinDateTimeToTry > dailyTime )
+				dateUsedToBuildDateTimeCandidate =
+					dateUsedToBuildDateTimeCandidate.AddDays( 1 );
+			DateTime dateTimeCandidate = new DateTime(
+				dateUsedToBuildDateTimeCandidate.Year ,
+				dateUsedToBuildDateTimeCandidate.Month ,
+				dateUsedToBuildDateTimeCandidate.Day ,
+				dailyTime.Hour ,
+				dailyTime.Minute ,
+				dailyTime.Second );
+			return dateTimeCandidate;
+		}
+//		private bool isCandidateTooFarAhead(
+//			DateTime startingDateTime , DateTime dateTimeCandidate ,
+//			int maxNumberOfDaysToLookAheadFor )
+//		{
+//			bool isTooFarAhead;
+//			TimeSpan timeSpan = dateTimeCandidate.Subtract( startingDateTime );
+//			isTooFarAhead = ( timeSpan.Days > maxNumberOfDaysToLookAheadFor );
+//			return isTooFarAhead;
+//		}
+		/// <summary>
+		/// Starting from startingDateTime, this method searches for the next
+		/// dateTime when the benchmark is traded at time dailyTime.
+		/// The returned value is greater than or equal to startingDateTime,
+		/// that is, startingDateTime itself is a valid candidate.
+		/// If no solution is found within maxNumberOfDaysToLookAheadFor days,
+		/// then an exception is returned
+		/// </summary>
+		/// <param name="startingDateTime"></param>
+		/// <param name="dailyTime"></param>
+		/// <param name="maxNumberOfDaysToLookAheadFor"></param>
+		/// <returns></returns>
+		public virtual DateTime GetThisOrNextDateTimeWhenTraded(
+			DateTime startingDateTime ,
+			Time dailyTime ,
+			DateTime lastAcceptableDateTime )
+		{
+			DateTime dateTimeCandidate = this.getDateTimeCandidate(
+				startingDateTime , dailyTime );
+			DateTime thisOrNextDateTimeWhenTraded = DateTime.MinValue;
+			while ( dateTimeCandidate <= lastAcceptableDateTime &&
+//				!this.isCandidateTooFarAhead(
+//				startingDateTime , dateTimeCandidate , maxNumberOfDaysToLookAheadFor ) &&
+			       ( thisOrNextDateTimeWhenTraded == DateTime.MinValue ) )
+			{
+				// a valid dateTime when traded has not been found yet
+				if ( this.isExchanged( dateTimeCandidate ) )
+					thisOrNextDateTimeWhenTraded = dateTimeCandidate;
+				else
+					dateTimeCandidate = this.getDateTimeCandidate(
+						dateTimeCandidate.AddSeconds( 1 ) , dailyTime );
+			}
+			if ( thisOrNextDateTimeWhenTraded == DateTime.MinValue )
+				throw new Exception(
+					"In the specified number of days, the benchmark is never " +
+					"traded at the specified dailyTime!" );
+			return thisOrNextDateTimeWhenTraded;
+		}
+		#endregion GetThisOrNextDateTimeWhenTraded
 		
 	}
 }
