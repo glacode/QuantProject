de91eb8d6c2febb1b57237f48f8134b85a0f56b3
glauco_1 <>
2010-03-28 13:17:21 +0000

    Returns the WeightedPositions to be opened out of sample. Returns null if none is suitable

---
+++ QuantProject/b4_Business/a2_Strategies/OutOfSample/IEntryStrategy.cs
@@ -0,0 +1,40 @@
+/*
+QuantProject - Quantitative Finance Library
+
+IEntryStrategy.cs
+Copyright (C) 2010
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+using System;
+
+namespace QuantProject.Business.Strategies.OutOfSample
+{
+	/// <summary>
+	/// Returns the WeightedPositions to be opened out of sample.
+	/// Returns null if none is suitable
+	/// </summary>
+	public interface IEntryStrategy
+	{
+		/// <summary>
+		/// Returns the WeightedPositions to be opened out of sample.
+		/// Returns null if none is suitable
+		/// </summary>
+		/// <returns>null if it finds no positions as required</returns>
+		WeightedPositions GetPositionsToBeOpened();
+	}
+}
