8e8a91ae2e5079f0e0c81b459964c510d212e124
glauco_1 <>
2010-03-28 14:39:20 +0000

    If the weightedPositions are null then hashCodeForTickerComposition is the empty string

--- QuantProject/b4_Business/a2_Strategies/OutOfSample/TestingPositions.cs
+++ QuantProject/b4_Business/a2_Strategies/OutOfSample/TestingPositions.cs
@@ -18,7 +18,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
+ */
 
 using System;
 using System.Collections;
@@ -27,137 +27,143 @@ using QuantProject.Business.Strategies;
 
 namespace QuantProject.Business.Strategies.OutOfSample
 {
-  /// <summary>
-  /// This is the base class representing positions that 
-  /// should be tested out of sample in a back test
-  /// Every strategy should test out of sample an object
-  /// of this type or an object derived from this class
-  /// </summary>
-  [Serializable]
-  public class TestingPositions
-  {
-  	private WeightedPositions weightedPositions;
-  	private string hashCodeForTickerComposition;
-  	private string hashCode;
-  	private double fitnessInSample;
-  	  	
+	/// <summary>
+	/// This is the base class representing positions that
+	/// should be tested out of sample in a back test
+	/// Every strategy should test out of sample an object
+	/// of this type or an object derived from this class
+	/// </summary>
+	[Serializable]
+	public class TestingPositions
+	{
+		private WeightedPositions weightedPositions;
+		private string hashCodeForTickerComposition;
+		private string hashCode;
+		private double fitnessInSample;
+		
 		public WeightedPositions WeightedPositions
 		{
 			get{return this.weightedPositions;}
 		}
-    public double FitnessInSample
+		public double FitnessInSample
 		{
 			get{return this.fitnessInSample;}
 			set{this.fitnessInSample = value;}
 		}
 		
 		public string HashCodeForTickerComposition
-    {
-      get
-      {
-      	if(this.hashCodeForTickerComposition == null && 
-      	   this.weightedPositions != null)
-      	//if hashCodeForTickerComposition has not been computed yet and
-      	//the current instance is not empty
-      	{
-      		ArrayList listOfTickers =
-      			new ArrayList(this.weightedPositions.SignedTickers.Tickers);
-		      listOfTickers.Sort();
-		      foreach(string tickerCode in listOfTickers)
-		        this.hashCodeForTickerComposition += tickerCode + ";";
-      	}
-      	return this.hashCodeForTickerComposition;
+		{
+			get
+			{
+				if ( this.hashCodeForTickerComposition == null )
+				{
+					if ( this.weightedPositions != null)
+						//if hashCodeForTickerComposition has not been computed yet and
+						//the current instance is not empty
+					{
+						ArrayList listOfTickers =
+							new ArrayList(this.weightedPositions.SignedTickers.Tickers);
+						listOfTickers.Sort();
+						foreach(string tickerCode in listOfTickers)
+							this.hashCodeForTickerComposition += tickerCode + ";";
+					}
+					else
+						// hashCodeForTickerComposition has not been computed yet, but
+						// the current instance is empty
+						this.hashCodeForTickerComposition = "";
+				}
+				return this.hashCodeForTickerComposition;
 			}
-    }
+		}
 		/// <summary>
-	  /// Hash code for the current instance
-	  /// Two instances have the same Hash code iif 
-	  /// they have the same tickers with the same signed weights
-	  /// </summary>
+		/// Hash code for the current instance
+		/// Two instances have the same Hash code iif
+		/// they have the same tickers with the same signed weights
+		/// </summary>
 		public string HashCode
-    {
-      get
-      {
-      	if(this.hashCode == null && 
-      	   this.weightedPositions != null)
-      	//if hashCode has not been computed yet and
-      	//the current instance is not empty
-      	{
-      		ArrayList listOfTickers =
-      			new ArrayList(this.weightedPositions.SignedTickers.Tickers);
-		      listOfTickers.Sort();
-		      foreach(string tickerCode in listOfTickers)
-		        this.hashCode += 
-		      		tickerCode + "_" +
-		      		this.weightedPositions.GetWeightedPosition(tickerCode).Weight.ToString() +
-		      		";";
-      	}
-      	return this.hashCode;
+		{
+			get
+			{
+				if(this.hashCode == null &&
+				   this.weightedPositions != null)
+					//if hashCode has not been computed yet and
+					//the current instance is not empty
+				{
+					ArrayList listOfTickers =
+						new ArrayList(this.weightedPositions.SignedTickers.Tickers);
+					listOfTickers.Sort();
+					foreach(string tickerCode in listOfTickers)
+						this.hashCode +=
+							tickerCode + "_" +
+							this.weightedPositions.GetWeightedPosition(tickerCode).Weight.ToString() +
+							";";
+				}
+				return this.hashCode;
 			}
-    }
+		}
 		public bool OnlyLongPositions
-    {
-      get
-      {
-      	bool returnValue = false;
-      	int numOfWeightedPositions = this.weightedPositions.Count;
-      	int numOfLongPositions = this.weightedPositions.NumberOfLongPositions;
-      	if(numOfLongPositions == numOfWeightedPositions)
-      	//there are only long positions
-      			returnValue = true;
-      	
-      	return returnValue;
+		{
+			get
+			{
+				bool returnValue = false;
+				int numOfWeightedPositions = this.weightedPositions.Count;
+				int numOfLongPositions = this.weightedPositions.NumberOfLongPositions;
+				if(numOfLongPositions == numOfWeightedPositions)
+					//there are only long positions
+					returnValue = true;
+				
+				return returnValue;
 			}
-    }
+		}
 		public bool OnlyShortPositions
-    {
-      get
-      {
-      	bool returnValue = false;
-      	int numOfWeightedPositions = this.weightedPositions.Count;
-      	int numOfShortPositions = this.weightedPositions.NumberOfShortPositions;
-      	if(numOfShortPositions == numOfWeightedPositions)
-      	//there are only short positions
-      			returnValue = true;
-      	
-      	return returnValue;
+		{
+			get
+			{
+				bool returnValue = false;
+				int numOfWeightedPositions = this.weightedPositions.Count;
+				int numOfShortPositions = this.weightedPositions.NumberOfShortPositions;
+				if(numOfShortPositions == numOfWeightedPositions)
+					//there are only short positions
+					returnValue = true;
+				
+				return returnValue;
 			}
-    }
+		}
 		public bool BothLongAndShortPositions
-    {
-      get
-      {
-      	bool returnValue = false;
-      	int numOfWeightedPositions = this.weightedPositions.Count;
-      	int numOfShortPositions = this.weightedPositions.NumberOfShortPositions;
-      	int numOfLongPositions = this.weightedPositions.NumberOfLongPositions;
-      	if(numOfShortPositions != numOfWeightedPositions &&
-      	   numOfLongPositions != numOfWeightedPositions)
-      	//there are both long and short positions
-      			returnValue = true;
-      	
-      	return returnValue;
+		{
+			get
+			{
+				bool returnValue = false;
+				int numOfWeightedPositions = this.weightedPositions.Count;
+				int numOfShortPositions = this.weightedPositions.NumberOfShortPositions;
+				int numOfLongPositions = this.weightedPositions.NumberOfLongPositions;
+				if(numOfShortPositions != numOfWeightedPositions &&
+				   numOfLongPositions != numOfWeightedPositions)
+					//there are both long and short positions
+					returnValue = true;
+				
+				return returnValue;
 			}
-    }
+		}
 		
 		public TestingPositions(WeightedPositions weightedPositions)
-    {
- 			this.weightedPositions = weightedPositions;
- 			this.fitnessInSample = double.MinValue;
- 		}
+		{
+			this.weightedPositions = weightedPositions;
+			this.fitnessInSample = double.MinValue;
+		}
 		
 		public TestingPositions(WeightedPositions weightedPositions,
 		                        double fitnessInSample)
-    {
- 			this.weightedPositions = weightedPositions;
- 			this.fitnessInSample = fitnessInSample;
- 		}
+		{
+			this.weightedPositions = weightedPositions;
+			this.fitnessInSample = fitnessInSample;
+		}
 		
 		//it creates an empty TestingPositions
 		public TestingPositions()
-    {
- 			this.weightedPositions = null;
- 			this.fitnessInSample = double.MinValue;
+		{
+			this.weightedPositions = null;
+			this.fitnessInSample = double.MinValue;
 		}
 	}
 }
