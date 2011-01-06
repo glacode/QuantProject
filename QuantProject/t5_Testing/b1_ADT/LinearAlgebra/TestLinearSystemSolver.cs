37ff7eed3282c0202ee16789bda6ea6481f8f38f
glauco_1 <>
2011-01-06 19:34:55 +0000

    Copyright notice has been added

--- QuantProject/t5_Testing/b1_ADT/LinearAlgebra/TestLinearSystemSolver.cs
+++ QuantProject/t5_Testing/b1_ADT/LinearAlgebra/TestLinearSystemSolver.cs
@@ -1,10 +1,23 @@
 /*
- * Created by SharpDevelop.
- * User: Glauco
- * Date: 3/21/2010
- * Time: 8:10 PM
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
+QuantProject - Quantitative Finance Library
+
+TestLinearSystemSolver.cs
+Copyright (C) 2010
+Glauco Siliprandi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
 using System;
