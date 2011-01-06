a00b95ac003c6372f170f92efb0454ee0c58adf9
glauco_1 <>
2011-01-06 19:28:22 +0000

    now it has been actually written: analyzes a LinearRegressionTestingPositions that has been logged in sample

--- QuantProject/b7_Scripts/WalkForwardTesting/LinearRegression/Logging/AnalyzerForLinearRegressionTestingPositions.cs
+++ QuantProject/b7_Scripts/WalkForwardTesting/LinearRegression/Logging/AnalyzerForLinearRegressionTestingPositions.cs
@@ -21,8 +21,16 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
 using System;
+using System.Drawing;
+using System.Windows.Forms;
 
+using QuantProject.ADT.Histories;
+using QuantProject.ADT.Statistics;
+using QuantProject.Business.DataProviders;
 using QuantProject.Business.Scripting;
+using QuantProject.Business.Strategies.ReturnsManagement.Time;
+using QuantProject.Business.Strategies.ReturnsManagement.Time.IntervalsSelectors;
+using QuantProject.Presentation;
 
 namespace QuantProject.Scripts.WalkForwardTesting.LinearRegression
 {
@@ -33,26 +41,204 @@ namespace QuantProject.Scripts.WalkForwardTesting.LinearRegression
 	[Serializable]
 	public class AnalyzerForLinearRegressionTestingPositions : IExecutable
 	{
-		LinearRegressionTestingPositions linearRegressionTestingPositions;
-		DateTime accountTimerDateTimeWhenThisObjectWasLogged;
+		private LinearRegressionTestingPositions linearRegressionTestingPositions;
+		private DateTime firstDateTimeInSample;
+		private IIntervalsSelector intervalsSelector;
+		private IReturnIntervalSelectorForSignaling returnIntervalSelectorForSignaling;
+		private DateTime accountTimerDateTimeWhenThisObjectWasLogged;
 		
+		/// <summary>
+		/// Generation when the TestingPositions object has been created
+		/// (if genetically optimized)
+		/// </summary>
+		public int Generation
+		{
+			get
+			{
+				return this.linearRegressionTestingPositions.Generation;
+			}
+		}
+		public double FitnessInSample
+		{
+			get { return this.linearRegressionTestingPositions.FitnessInSample; }
+		}
+
 		public AnalyzerForLinearRegressionTestingPositions(
 			LinearRegressionTestingPositions linearRegressionTestingPositions ,
-//			int numberOfInSampleDays ,
-			DateTime accountTimerDateTimeWhenThisObjectWasLogged )
+			DateTime accountTimerDateTimeWhenThisObjectWasLogged
+		)
 		{
 			this.linearRegressionTestingPositions = linearRegressionTestingPositions;
+//			this.firstDateTimeInSample = firstDateTimeInSample;
+//			this.intervalsSelector = intervalsSelector;
+//			this.returnIntervalSelectorForSignaling = returnIntervalSelectorForSignaling;
 			this.accountTimerDateTimeWhenThisObjectWasLogged =
 				accountTimerDateTimeWhenThisObjectWasLogged;
 		}
+		
+		public void SetOtherMembers(
+			DateTime firstDateTimeInSample ,
+			IIntervalsSelector intervalsSelector ,
+			IReturnIntervalSelectorForSignaling returnIntervalSelectorForSignaling )
+		{
+			this.firstDateTimeInSample = firstDateTimeInSample;
+			this.intervalsSelector = intervalsSelector;
+			this.returnIntervalSelectorForSignaling = returnIntervalSelectorForSignaling;
+		}
+		
+		#region Run
+		
+		#region getReturnIntervals
+		private ReturnIntervals initializeReturnIntervalsWithTheFirstInterval(
+			DateTime firstDateTime )
+		{
+			ReturnInterval firstReturnInterval =
+				this.intervalsSelector.GetFirstInterval( firstDateTime );
+			ReturnIntervals returnIntervals = new ReturnIntervals( firstReturnInterval );
+			return returnIntervals;
+		}
+		private DateTime getFirstDateTime()
+		{
+			DateTime firstDateTime = this.accountTimerDateTimeWhenThisObjectWasLogged.AddDays( - 180 );
+			return firstDateTime;
+		}
+		private DateTime getLastDateTime()
+		{
+			DateTime lastDateTime = this.accountTimerDateTimeWhenThisObjectWasLogged;
+			return lastDateTime;
+		}
+		private ReturnInterval getNextInterval( ReturnIntervals returnIntervals )
+		{
+			ReturnInterval returnIntervalForTrading = this.intervalsSelector.GetNextInterval(
+				returnIntervals );
+//			ReturnInterval returnIntervalForSignaling =
+//				this.returnIntervalSelectorForSignaling.GetReturnIntervalUsedForSignaling(
+//					returnIntervalForTrading );
+//			return returnIntervalForSignaling;
+			return returnIntervalForTrading;
+		}
+		private ReturnIntervals getReturnIntervals(
+			DateTime firstDateTime , DateTime lastDateTime )
+		{
+			ReturnIntervals returnIntervals =
+				this.initializeReturnIntervalsWithTheFirstInterval( firstDateTime );
+			while ( returnIntervals.LastInterval.End < lastDateTime )
+			{
+				ReturnInterval nextInterval = this.getNextInterval( returnIntervals );
+				returnIntervals.Add( nextInterval );
+			}
+			return returnIntervals;
+		}
+		private ReturnIntervals getReturnIntervals()
+		{
+//			DateTime firstDateTime = this.getFirstDateTime();
+			DateTime firstDateTime = this.firstDateTimeInSample;
+			DateTime lastDateTime = this.getLastDateTime();
+			ReturnIntervals returnIntervals = this.getReturnIntervals(
+				firstDateTime , lastDateTime );
+			return returnIntervals;
+		}
+		#endregion getReturnIntervals
+		
+		private History getVirtualValues(
+			ReturnIntervals returnIntervals , IVirtualReturnComputer virtualReturnComputer )
+		{
+			History virtualValues = new History();
+			int currentIntervalIndex = 0;
+			double currentValue = 30000;
+			while ( currentIntervalIndex < returnIntervals.Count )
+			{
+				ReturnInterval currentInterval = returnIntervals[ currentIntervalIndex ];
+				if ( !virtualValues.ContainsKey( currentInterval.Begin ) )
+					virtualValues.Add( currentInterval.Begin , currentValue );
+				try
+				{
+					double currentReturn = virtualReturnComputer.ComputeReturn( currentInterval );
+					currentValue = currentValue + currentValue * currentReturn;
+					virtualValues.Add( currentInterval.End , currentValue );
+				}
+				catch( TickerNotExchangedException )
+				{
+				}
+				currentIntervalIndex++;
+			}
+			return virtualValues;
+		}
+		
+		#region getVirtualValues
+		private void getVirtualValues(
+			ReturnIntervals returnIntervals ,
+			out History predictedVirtualValues , out History tradingPortfolioVirtualValues )
+		{
+			IHistoricalMarketValueProvider historicalMarketValueProvider =
+				new HistoricalAdjustedQuoteProvider();
+			
+			ReturnPredictor returnPredictor = new ReturnPredictor(
+				this.linearRegressionTestingPositions , this.returnIntervalSelectorForSignaling ,
+				historicalMarketValueProvider );
+			predictedVirtualValues = this.getVirtualValues(
+				returnIntervals , returnPredictor );
+			
+			PortfolioReturnComputer tradingPortfolioReturnComputer =
+				new PortfolioReturnComputer(
+					this.linearRegressionTestingPositions.TradingPortfolio ,
+					historicalMarketValueProvider );
+			tradingPortfolioVirtualValues = this.getVirtualValues(
+				returnIntervals , tradingPortfolioReturnComputer );
+		}
+		#endregion getVirtualValues
+		
+		#region showHistoriesPlots
+		private HistoriesViewer showHistoriesPlots(
+			History predictedVirtualValues , History tradingPortfolioVirtualValues )
+		{
+			HistoriesViewer historiesViewer =
+				new HistoriesViewer( "Linear Regression Log Analyzer" );
+			historiesViewer.StartPosition = FormStartPosition.Manual;
+			historiesViewer.Location = new Point( 200 , 200 );
+			historiesViewer.Add( predictedVirtualValues , Color.Red );
+			historiesViewer.Add( tradingPortfolioVirtualValues , Color.Green );
+			historiesViewer.Show();
+			return historiesViewer;
+		}
+		#endregion showHistoriesPlots
+		
+		#region showCorrelation
+		private double[] getArrayFromHistory( History history )
+		{
+			double[] arrayValues = new double[ history.Count ];
+			for( int i = 0 ; i < arrayValues.Length ; i++ )
+				arrayValues[ i ] = (double)history.GetValueList()[ i ];
+			return arrayValues;			
+		}
+
+		private void showCorrelation(
+			History predictedVirtualValues , History tradingPortfolioVirtualValues )
+		{
+			double correlationValue = BasicFunctions.PearsonCorrelationCoefficient(
+				this.getArrayFromHistory( predictedVirtualValues ) ,
+				this.getArrayFromHistory( tradingPortfolioVirtualValues ) );
+			MessageBox.Show( "Correlation value: " + correlationValue );
+		}
+		#endregion showCorrelation
+		
+		
 		public void Run()
 		{
 			// an idea would be to show some t-statistics for the
 			// coefficients and then show which are those who seem to
 			// be more significant
-			System.Windows.Forms.MessageBox.Show(
-				"A good analyzer has not yet been thought about, for " +
-				"a LinearRegressionTestingPositions" );
+			ReturnIntervals returnIntervals = this.getReturnIntervals();
+			
+			History predictedVirtualValues;
+			History tradingPortfolioVirtualValues;
+			this.getVirtualValues(
+				returnIntervals , out predictedVirtualValues , out tradingPortfolioVirtualValues );
+			
+			this.showHistoriesPlots( predictedVirtualValues , tradingPortfolioVirtualValues );
+			
+			this.showCorrelation( predictedVirtualValues , tradingPortfolioVirtualValues );
 		}
+		#endregion Run
 	}
 }
