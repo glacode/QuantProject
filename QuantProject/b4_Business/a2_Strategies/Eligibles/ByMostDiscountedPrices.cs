f07ed4d80eafa4a624fde655e6fca99a2e7b0d54
millo_1 <>
2011-01-16 18:34:18 +0000

    Updated ByMostDiscountedPrices IEligiblesSelectors

--- QuantProject/b4_Business/a2_Strategies/Eligibles/ByMostDiscountedPrices.cs
+++ QuantProject/b4_Business/a2_Strategies/Eligibles/ByMostDiscountedPrices.cs
@@ -28,7 +28,7 @@ using QuantProject.ADT.Histories;
 using QuantProject.ADT.Messaging;
 using QuantProject.Data.DataTables;
 using QuantProject.Data.Selectors;
-using QuantProject.Business.Financial.Fundamentals;
+using QuantProject.Business.Financial.Fundamentals.FairValueProviders;
 
 namespace QuantProject.Business.Strategies.Eligibles
 {
@@ -39,13 +39,17 @@ namespace QuantProject.Business.Strategies.Eligibles
 	/// are selected (the group can be "temporized": that is tickers
 	/// are returned depending on the time the selection is requested:
 	/// the group SP 500 should be like that);
-	/// -step 2: from tickers selected by step 1, the ones
+	/// -step 2: from tickers selected by step 1, only the ones
+	/// that have incomes greater than a given minimum in the 
+	/// last given times, in a row
+	/// -step 3: from tickers selected by step 2, the ones
 	/// (not more than a given max number) that have
-	/// the greatest (negative) relative difference between the current price
-	/// and the fair price;
+	/// the greatest relative difference between the fair price 
+	/// (provided by a IFairValueProvider) and the average price
+	/// computed for a given number of days
 	/// </summary>
 	[Serializable]
-	public class ByMostDiscountedPrices : IEligiblesSelector
+	public class ByMostDiscountedPrices :  IEligiblesSelector
 	{
 		public event NewMessageEventHandler NewMessage;
 		
@@ -54,6 +58,11 @@ namespace QuantProject.Business.Strategies.Eligibles
 		private int maxNumberOfEligibleTickersToBeChosen;
 		private int numOfDaysForFundamentalAnalysis;
 		private IFairValueProvider fairValueProvider;
+		private int numDaysForFundamentalDataAvailability;
+		private double minimumIncome;
+		private int numberOfMinGrowingIncomesInARowForTickers;
+		private double minimumRelativeDifferenceBetweenFairAndAverageMarketPrice;
+		private	int numOfDaysForAverageMarketPriceComputation;
 		
 		public string Description
 		{
@@ -67,7 +76,11 @@ namespace QuantProject.Business.Strategies.Eligibles
 				
 		public ByMostDiscountedPrices(IFairValueProvider fairValueProvider,
 			string tickersGroupID , bool temporizedGroup, 
-			int maxNumberOfEligibleTickersToBeChosen, int numOfDaysForFundamentalAnalysis)
+			int maxNumberOfEligibleTickersToBeChosen, int numOfDaysForFundamentalAnalysis,
+			int numDaysForFundamentalDataAvailability, double minimumIncome,
+			int numberOfMinGrowingIncomesInARowForTickers,
+			double minimumRelativeDifferenceBetweenFairAndAverageMarketPrice,
+			int numOfDaysForAverageMarketPriceComputation)
 		{
 			this.fairValueProvider = fairValueProvider;
 			this.temporizedGroup = temporizedGroup;
@@ -75,34 +88,61 @@ namespace QuantProject.Business.Strategies.Eligibles
 				maxNumberOfEligibleTickersToBeChosen;
 			this.tickersGroupID = tickersGroupID;
 			this.numOfDaysForFundamentalAnalysis = numOfDaysForFundamentalAnalysis;
+			this.numDaysForFundamentalDataAvailability = 
+				numDaysForFundamentalDataAvailability;
+			this.minimumIncome = minimumIncome;
+			this.numberOfMinGrowingIncomesInARowForTickers = 
+				numberOfMinGrowingIncomesInARowForTickers;
+			this.minimumRelativeDifferenceBetweenFairAndAverageMarketPrice =
+				minimumRelativeDifferenceBetweenFairAndAverageMarketPrice;
+			this.numOfDaysForAverageMarketPriceComputation =
+				numOfDaysForAverageMarketPriceComputation;
 		}
 		
 		private EligibleTickers getEligibleTickers_actually_getTableOfMostDiscountedTickers(
 			DataTable initialTableFromWhichToChooseTheMostDiscountedTickers, DateTime date )
 		{
+			double signOfCurrentFairPrice;
+			double currentFairPrice;
 			DataTable tableOfEligibleTickers;
 			if(!initialTableFromWhichToChooseTheMostDiscountedTickers.Columns.Contains("FairPrice"))
         initialTableFromWhichToChooseTheMostDiscountedTickers.Columns.Add("FairPrice", System.Type.GetType("System.Double"));
-			if(!initialTableFromWhichToChooseTheMostDiscountedTickers.Columns.Contains("MarketPrice"))
-        initialTableFromWhichToChooseTheMostDiscountedTickers.Columns.Add("LastMonthAverageMarketPrice", System.Type.GetType("System.Double"));
+			if(!initialTableFromWhichToChooseTheMostDiscountedTickers.Columns.Contains("AverageMarketPrice"))
+        initialTableFromWhichToChooseTheMostDiscountedTickers.Columns.Add("AverageMarketPrice", System.Type.GetType("System.Double"));
 			if(!initialTableFromWhichToChooseTheMostDiscountedTickers.Columns.Contains("RelativeDifferenceBetweenFairAndMarketPrice"))
         initialTableFromWhichToChooseTheMostDiscountedTickers.Columns.Add("RelativeDifferenceBetweenFairAndMarketPrice", System.Type.GetType("System.Double"));
       foreach(DataRow row in initialTableFromWhichToChooseTheMostDiscountedTickers.Rows)
       {
-      	row["FairPrice"] = 
-      		this.fairValueProvider.GetFairValue( (string)row[0],
-      	                                       date.AddDays(-this.numOfDaysForFundamentalAnalysis),
-      	                                       date );
-      	float[] lastMonthQuotes = 
-      		Quotes.GetArrayOfAdjustedCloseQuotes((string)row[0], date.AddDays(-30), date);
-      	row["LastMonthAverageMarketPrice"] = 
-      		ADT.Statistics.BasicFunctions.SimpleAverage(lastMonthQuotes);
-      	row["RelativeDifferenceBetweenFairAndMarketPrice"] = 
-      		((double)row["FairPrice"]-(double)row["LastMonthAverageMarketPrice"])/(double)row["FairPrice"];
+      	try{
+	      	currentFairPrice = this.fairValueProvider.GetFairValue( (string)row[0],
+	      	                                       date.AddDays(-this.numOfDaysForFundamentalAnalysis),
+	      	                                       date );
+	      	row["FairPrice"] = currentFairPrice;
+	      	double[] quotesForAveragePriceComputation = 
+	      		Quotes.GetDoubleArrayOfAdjustedCloseQuotes((string)row[0], date.AddDays(-this.numOfDaysForAverageMarketPriceComputation), date);
+	      	row["AverageMarketPrice"] = 
+	      		ADT.Statistics.BasicFunctions.SimpleAverage(quotesForAveragePriceComputation);
+	      	if(currentFairPrice < 0.0)
+	      		signOfCurrentFairPrice = -1.0;
+	      	else
+	      		signOfCurrentFairPrice = 1.0;
+	      	row["RelativeDifferenceBetweenFairAndMarketPrice"] = signOfCurrentFairPrice *
+	      		((double)row["FairPrice"]-(double)row["AverageMarketPrice"])/(double)row["AverageMarketPrice"];
+      	}
+      	catch(Exception ex)
+      	{
+					string s = ex.ToString();
+				}	
       }
-      tableOfEligibleTickers = 
+      string filterString = 
+      	"AverageMarketPrice is not null AND FairPrice is not null AND " +
+      	"RelativeDifferenceBetweenFairAndMarketPrice >" +
+      	this.minimumRelativeDifferenceBetweenFairAndAverageMarketPrice.ToString();
+      	
+      tableOfEligibleTickers =
       	ExtendedDataTable.CopyAndSort(initialTableFromWhichToChooseTheMostDiscountedTickers,
-      	                              "RelativeDifferenceBetweenFairAndMarketPrice", true);
+      	                              filterString,
+      	                              "RelativeDifferenceBetweenFairAndMarketPrice", false);
       ExtendedDataTable.DeleteRows(tableOfEligibleTickers, this.maxNumberOfEligibleTickersToBeChosen);
       
       DataColumn[] columnPrimaryKeys = new DataColumn[1];
@@ -126,8 +166,15 @@ namespace QuantProject.Business.Strategies.Eligibles
       else//the group is not temporized
       	group = new SelectorByGroup(this.tickersGroupID);
       DataTable tickersFromGroup = group.GetTableOfSelectedTickers();
+      SelectorByNumOfMinGrowingIncomesInARow selectorByMinimumGrowingIncomes =
+      	new SelectorByNumOfMinGrowingIncomesInARow(tickersFromGroup, history.FirstDateTime,
+      	                                            history.LastDateTime, 
+      	                                            this.minimumIncome, this.numberOfMinGrowingIncomesInARowForTickers,
+      	                                            12, this.numDaysForFundamentalDataAvailability);
+      DataTable tickersWithPositiveGrowingIncomes = 
+      	selectorByMinimumGrowingIncomes.GetTableOfSelectedTickers();
       
-      return this.getEligibleTickers_actually_getTableOfMostDiscountedTickers(tickersFromGroup, currentDate);
+      return this.getEligibleTickers_actually_getTableOfMostDiscountedTickers(tickersWithPositiveGrowingIncomes, currentDate);
       
 		}
 		
